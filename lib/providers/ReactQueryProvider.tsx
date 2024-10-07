c27f189d44064d893f98b6846ba05cee073ae305
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-10-07 11:47:20 +0200

    Add react-query-devtools
    In order to be able to debug react-query

--- lib/providers/ReactQueryProvider.tsx
+++ lib/providers/ReactQueryProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
 function ReactQueryProvider({ children }: React.PropsWithChildren) {
@@ -9,6 +10,7 @@ function ReactQueryProvider({ children }: React.PropsWithChildren) {
   return (
     <QueryClientProvider client={client}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
