danskernesdigitalebibliotek/go-frontend

Adding example of a way to use xstate with search filters
Using delayed async mocked data for a quick experimentation.

app/filters/getFilters.ts
@@ -1,0 +1,62 @@
+import { sleep } from "sleep-ts";
+
+export const filters = {
+  animal: ["monkey", "squirrel"],
+  food: ["banana", "acorn"]
+};
+
+export type TFilters = Partial<typeof filters>;
+
+// This function is a mock API call that simulates a delay in fetching data.
+// And it has fake data.
+const getFilters = (currentFilters: TFilters): Promise<{ data: TFilters }> =>
+  new Promise(async (resolve) => {
+    await sleep(1000);
+
+    if (
+      !currentFilters.animal ||
+      currentFilters.animal.length > 2 ||
+      !currentFilters.food ||
+      currentFilters.food.length > 2
+    ) {
+      resolve({
+        data: filters
+      });
+    }
+    if (
+      (currentFilters.animal &&
+        currentFilters.animal.length === 1 &&
+        currentFilters.animal[0] === "monkey") ||
+      (currentFilters.food &&
+        currentFilters.food.length === 1 &&
+        currentFilters.food[0] === "banana")
+    ) {
+      resolve({
+        data: {
+          animal: ["monkey"],
+          food: ["banana"]
+        }
+      });
+    }
+    if (
+      (currentFilters.animal &&
+        currentFilters.animal.length === 1 &&
+        currentFilters.animal[0] === "squirrel") ||
+      (currentFilters.food &&
+        currentFilters.food.length === 1 &&
+        currentFilters.food[0] === "acorn")
+    ) {
+      resolve({
+        data: {
+          animal: ["squirrel"],
+          food: ["acorn"]
+        }
+      });
+    }
+
+    resolve({
+      data: filters
+    });
+  });
+
+export default getFilters;

app/filters/machine.filters.ts
@@ -1,0 +1,68 @@
+import { assign, fromPromise, setup } from "xstate";
+
+import getFilters, { filters, TFilters } from "./getFilters";
+
+export const fetchMachine = setup({
+  types: {
+    context: {} as {
+      data: TFilters | null;
+      currentFilters: TFilters | null;
+    },
+    events: {} as
+      | {
+          type: "SEARCH";
+          value: TFilters;
+        }
+      | {
+          type: "RESET";
+        }
+  },
+  actors: {
+    fetchFilters: fromPromise(({ input }: { input: TFilters }) =>
+      getFilters(input)
+    )
+  }
+}).createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QAoC2BDAxgCwJYDswBKAOlwgBswBiAZQFEBBAJQGEAJAbQAYBdRUAAcA9rFwAXXMPwCQAD0QBGACwAOEgDZuigKwB2VYr3buAJmV6ANCACeSvcpIBOHQGYVb18qd6nT1QC+AdZoWHiEpORU1Mz0DAAqPPxIICJiktKyCggAtMo6JK6m5nqm3F7eqla2iHmuzhqlrjrKaq56itrKQSEYOATEJABmuBTiYABOBFDUENJgZPgAbsIA1guh-RHDo+NT+FAIBCuY6Bn4SUmyaRJSMinZZfU6Gqo63KVaLk3WdrmuGhIqmaqicui0ymKKh6IE24UGIzGk2m1EmE2EExIggoZyGGNQJDhA1IiL20yOy2Ep3Olz411Et0yD0QnhIplB3DBpn05Wav0QZUUzlMPkhimKrncilcMKJ2yG6FGAFcJjQ5LBxGcFughntkJ1uNwiNQ5QjFRQVWArikbucsizQYVmk4NMpmuzXB9+QhdI5IT4Re9lIoNDpAjD8MIIHBZKaiPT0nd7bknPUiiUnq1-NU-jlpULlI1zCLpcVC+HemFiWRKGAE4z7qBsm4SDp3gZ-E41C8nKZvYonNxCoXlB9xbpXF29LK+vCSbtkQd63bmQginoSP6OkVe-4Xv2NEKdNKPse3mG291grDZ9WFcrVcuk6uOkODOLVG895L+y5hxpXDeYM3C-IIgiAA */
+  initial: "idle",
+  context: {
+    data: null,
+    currentFilters: filters
+  },
+  states: {
+    idle: {
+      on: {
+        SEARCH: {
+          actions: assign({ currentFilters: ({ event }) => event.value }),
+          target: "filtering"
+        },
+        RESET: {
+          actions: assign({ currentFilters: null, data: filters }),
+          target: "idle"
+        }
+      }
+    },
+    filtering: {
+      invoke: {
+        src: "fetchFilters",
+        input: ({
+          context
+        }: {
+          context: { currentFilters: TFilters | null };
+        }) => context.currentFilters ?? {},
+        onDone: {
+          target: "idle",
+          actions: assign({
+            data: ({ event }) => event.output.data
+          })
+        },
+        onError: "failure"
+      }
+    },
+    failure: {
+      after: {
+        1000: "filtering"
+      }
+    }
+  }
+});

inleadmedia/dpl-go

app/filters/getFilters.ts
@@ -1,0 +1,63 @@
+import { sleep } from "sleep-ts";
+
+export const allFilters = {
+  animal: ["monkey", "squirrel"],
+  food: ["banana", "acorn"]
+};
+
+export type TFilters = Partial<typeof allFilters>;
+export type TFilterKey = keyof TFilters;
+
+// This function is a mock API call that simulates a delay in fetching data.
+// And it has fake data.
+const getFilters = (currentFilters: TFilters): Promise<{ data: TFilters }> =>
+  new Promise<{ data: TFilters }>(async (resolve) => {
+    await sleep(1000);
+
+    if (
+      !currentFilters.animal ||
+      currentFilters.animal.length > 2 ||
+      !currentFilters.food ||
+      currentFilters.food.length > 2
+    ) {
+      resolve({
+        data: allFilters
+      });
+    }
+    if (
+      (currentFilters.animal &&
+        currentFilters.animal.length === 1 &&
+        currentFilters.animal[0] === "monkey") ||
+      (currentFilters.food &&
+        currentFilters.food.length === 1 &&
+        currentFilters.food[0] === "banana")
+    ) {
+      resolve({
+        data: {
+          animal: ["monkey"],
+          food: ["banana"]
+        }
+      });
+    }
+    if (
+      (currentFilters.animal &&
+        currentFilters.animal.length === 1 &&
+        currentFilters.animal[0] === "squirrel") ||
+      (currentFilters.food &&
+        currentFilters.food.length === 1 &&
+        currentFilters.food[0] === "acorn")
+    ) {
+      resolve({
+        data: {
+          animal: ["squirrel"],
+          food: ["acorn"]
+        }
+      });
+    }
+
+    resolve({
+      data: allFilters
+    });
+  });
+
+export default getFilters;

app/filters/machine.filters.ts
@@ -1,0 +1,68 @@
+import { assign, fromPromise, setup } from "xstate";
+
+import getFilters, { allFilters, TFilters } from "./getFilters";
+
+export const fetchMachine = setup({
+  types: {
+    context: {} as {
+      data: TFilters | null;
+      selectedFilters: TFilters | null;
+    },
+    events: {} as
+      | {
+          type: "SEARCH";
+          value: TFilters;
+        }
+      | {
+          type: "RESET";
+        }
+  },
+  actors: {
+    fetchFilters: fromPromise(({ input }: { input: TFilters }) =>
+      getFilters(input)
+    )
+  }
+}).createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QAoC2BDAxgCwJYDswBKAOlwgBswBiAZQFEBBAJQGEAJAbQAYBdRUAAcA9rFwAXXMPwCQAD0QBGACwAOEgDZuigKwB2VYr3buAJmV6ANCACeSvcpIBOHQGYVb18qd6nT1QC+AdZoWHiEpORU1Mz0DAAqPPxIICJiktKyCggAtMo6JK6m5nqm3F7eqla2iHmuzhqlrjrKaq56itrKQSEYOATEJABmuBTiYABOBFDUENJgZPgAbsIA1guh-RHDo+NT+FAIBCuY6Bn4SUmyaRJSMinZZfU6Gqo63KVaLk3WdrmuGhIqmaqicui0ymKKh6IE24UGIzGk2m1EmE2EExIggoZyGGNQJDhA1IiL20yOy2Ep3Olz411Et0yD0QnhIplB3DBpn05Wav0QZUUzlMPkhimKrncilcMKJ2yG6FGAFcJjQ5LBxGcFughntkJ1uNwiNQ5QjFRQVWArikbucsizQYVmk4NMpmuzXB9+QhdI5IT4Re9lIoNDpAjD8MIIHBZKaiPT0nd7bknPUiiUnq1-NU-jlpULlI1zCLpcVC+HemFiWRKGAE4z7qBsm4SDp3gZ-E41C8nKZvYonNxCoXlB9xbpXF29LK+vCSbtkQd63bmQginoSP6OkVe-4Xv2NEKdNKPse3mG291grDZ9WFcrVcuk6uOkODOLVG895L+y5hxpXDeYM3C-IIgiAA */
+  initial: "idle",
+  context: {
+    data: null,
+    selectedFilters: allFilters
+  },
+  states: {
+    idle: {
+      on: {
+        SEARCH: {
+          actions: assign({ selectedFilters: ({ event }) => event.value }),
+          target: "filtering"
+        },
+        RESET: {
+          actions: assign({ selectedFilters: null, data: allFilters }),
+          target: "idle"
+        }
+      }
+    },
+    filtering: {
+      invoke: {
+        src: "fetchFilters",
+        input: ({
+          context
+        }: {
+          context: { selectedFilters: TFilters | null };
+        }) => context.selectedFilters ?? {},
+        onDone: {
+          target: "idle",
+          actions: assign({
+            data: ({ event }) => event.output.data
+          })
+        },
+        onError: "failure"
+      }
+    },
+    failure: {
+      after: {
+        1000: "filtering"
+      }
+    }
+  }
+});