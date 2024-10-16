45b666a080a06e341cea95e48da72cfd7a3ca042
Adam Antal <adoantal@gmail.com>
2024-10-16 15:10:30 +0200

    Refactor global color variables and tailwind config
    - color variables to use hsla() so we can work with opacity
    - rename beige-dimmed to beige-overlay + same with blue-dimmed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -193,49 +193,49 @@ const config: Config = {
     extend: {
       colors: {
         background: {
-          DEFAULT: "hsl(var(--background))",
-          foreground: "hsl(var(--background-foreground))"
+          DEFAULT: "var(--background)",
+          overlay: "var(--background-overlay)"
         },
-        foreground: "hsl(var(--foreground))",
-        border: "hsl(var(--border))",
+        foreground: "var(--foreground)",
+        border: "var(--border)",
 
         // shadcn colors
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))"
+          DEFAULT: "var(--card)",
+          foreground: "var(--card-foreground)"
         },
         popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))"
+          DEFAULT: "var(--popover))",
+          foreground: "var(--popover-foreground)"
         },
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))"
+          DEFAULT: "var(--primary))",
+          foreground: "var(--primary-foreground)"
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))"
+          DEFAULT: "var(--secondary))",
+          foreground: "var(--secondary-foreground)"
         },
         muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))"
+          DEFAULT: "var(--muted))",
+          foreground: "var(--muted-foreground)"
         },
         accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))"
+          DEFAULT: "var(--accent))",
+          foreground: "var(--accent-foreground)"
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))"
+          DEFAULT: "var(--destructive))",
+          foreground: "var(--destructive-foreground)"
         },
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
+        input: "var(--input)",
+        ring: "var(--ring)",
         chart: {
-          "1": "hsl(var(--chart-1))",
-          "2": "hsl(var(--chart-2))",
-          "3": "hsl(var(--chart-3))",
-          "4": "hsl(var(--chart-4))",
-          "5": "hsl(var(--chart-5))"
+          "1": "var(--chart-1)",
+          "2": "var(--chart-2)",
+          "3": "var(--chart-3)",
+          "4": "var(--chart-4)",
+          "5": "var(--chart-5)"
         }
       },
       transitionDuration: {
@@ -264,7 +264,8 @@ const config: Config = {
         "grid-column-quarter": "calc(var(--grid-column)/4)"
       },
       boxShadow: {
-        button: "2px 2px 0 0px hsl(var(--foreground))"
+        button: "2px 2px 0 0px var(--foreground)",
+        buttonHover: "1px 1px 0 0px var(--foreground)"
       }
     }
   },
