41ab137a6df937ba656a0a1781d98570799f7145
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-10-22 09:38:13 +0200

    Redirect to user profile page after login

--- lib/session/oauth/uniloginClient.ts
+++ lib/session/oauth/uniloginClient.ts
@@ -1,4 +1,4 @@
-import { Issuer } from "openid-client";
+import { Issuer } from "openid-client"
 
 export const uniloginClientConfig = {
   wellKnownUrl: process.env.NEXT_PUBLIC_UNILOGIN_WELKNOWN_URL,
@@ -11,19 +11,17 @@ export const uniloginClientConfig = {
   post_logout_redirect_uri: `${process.env.NEXT_PUBLIC_APP_URL}`,
   response_type: "code",
   grant_type: "authorization_code",
-  post_login_route: `${process.env.NEXT_PUBLIC_APP_URL}`
-};
+  post_login_route: `${process.env.NEXT_PUBLIC_APP_URL}/user/profile`,
+}
 
 export async function getUniloginClient() {
-  const UniloginIssuer = await Issuer.discover(
-    uniloginClientConfig.wellKnownUrl!
-  );
+  const UniloginIssuer = await Issuer.discover(uniloginClientConfig.wellKnownUrl!)
   const client = new UniloginIssuer.Client({
     client_id: uniloginClientConfig.client_id!,
     client_secret: uniloginClientConfig.client_secret!,
     response_types: ["code"],
     redirect_uris: [uniloginClientConfig.redirect_uri],
-    token_endpoint_auth_method: "client_secret_post"
-  });
-  return client;
+    token_endpoint_auth_method: "client_secret_post",
+  })
+  return client
 }
