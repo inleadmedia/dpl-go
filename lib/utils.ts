4966ab269994d0b714f3ac9f9f16ee8db97c583b
thomasgross <thomasgrossrasmussen@gmail.com>
2024-10-28 10:54:44 +0100

    fix: cleanup unnecessary code

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,26 +2,8 @@ import { extendedTheme } from "@/tailwind.config"
 import { type ClassValue, clsx } from "clsx"
 import { extendTailwindMerge } from "tailwind-merge"
 
-function formatColors() {
-	const colors = []
-	for (const [key, color] of Object.entries(extendedTheme.extend.colors)) {
-		if (typeof color === 'string') {
-			colors.push(key)
-		} else {
-			const colorGroup = Object.keys(color).map(subKey =>
-				subKey === 'DEFAULT' ? '' : subKey,
-			)
-			colors.push({ [key]: colorGroup })
-		}
-	}
-	return colors
-}
-
 const customTwMerge = extendTailwindMerge({
   extend: {
-    theme: {
-      colors: formatColors(),
-    },
     classGroups: {
       'font-size': [
         {
