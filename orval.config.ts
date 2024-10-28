b76dc0bdcbd3525a625c3fda699e0d8bb27e04d0
Adam Antal <adoantal@gmail.com>
2024-10-28 11:25:51 +0100

    Add publizonAdapter configuration to orval.config.ts

--- orval.config.ts
+++ orval.config.ts
@@ -25,4 +25,35 @@ export default defineConfig({
       },
     },
   },
+  publizonAdapter: {
+    output: {
+      mode: "split",
+      target: "lib/publizon-api/publizon.ts",
+      schemas: "lib/publizon-api/model",
+      client: "react-query",
+      override: {
+        mutator: {
+          path: "lib/publizon-api/mutator/fetcher.ts",
+          name: "fetcher",
+        },
+        query: {
+          useQuery: true,
+        },
+        operations: {
+          // The reason why we add this here is to be able to use "enabled" option in the
+          // publizon adapter queries. This lets us call them conditionally.
+          getV1LoanstatusIdentifier: {
+            requestOptions: false,
+          },
+        },
+      },
+      prettier: true,
+    },
+    input: {
+      target: "lib/publizon-api/publizon-adapter.yaml",
+      converterOptions: {
+        indent: 2,
+      },
+    },
+  },
 })
