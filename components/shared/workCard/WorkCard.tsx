018c626fe5e9390810747518551878862298dfc5
thomasgross <thomasgrossrasmussen@gmail.com>
2024-10-29 13:40:07 +0100

    fix: rollback on work card to prevent merge conflicts

--- components/shared/workCard/WorkCard.tsx
+++ components/shared/workCard/WorkCard.tsx
@@ -13,28 +13,30 @@ type WorkCardProps = {
 
 const WorkCard = ({ work }: WorkCardProps) => {
   return (
-    <Link href={`/work/${work.workId}`}>
-      <div key={work.workId} className="rounded-sm bg-background-overlay p-2 md:p-4">
-        <div
-          className="px-auto relative mx-auto flex aspect-[166/228] w-full items-center rounded-sm shadow-coverPicture
-            md:mb-6 md:mt-9">
-          <Image
-            // TODO: dynamic image
-            src="https://i.pinimg.com/564x/b0/4a/b2/b04ab2de27591aee4a76015e42e0e282.jpg"
-            alt="work image"
-            layout="responsive"
-            width={166}
-            height={228}
-            className="overflow-hidden rounded-sm"
-          />
+    <div className="mb-4">
+      <Link href={`/work/${work.workId}`}>
+        <div key={work.workId} className="rounded-sm bg-background-overlay p-2 md:p-4">
+          <div
+            className="px-auto relative mx-auto mb-3 mt-6 flex aspect-[166/228] w-[calc(100%-76px-4px)] items-center
+              rounded-sm shadow-coverPicture md:mb-6 md:mt-9 md:w-[calc(100%-106px)]">
+            <Image
+              // TODO: dynamic image
+              src="https://i.pinimg.com/564x/b0/4a/b2/b04ab2de27591aee4a76015e42e0e282.jpg"
+              alt="work image"
+              layout="responsive"
+              width={166}
+              height={228}
+              className="overflow-hidden rounded-sm"
+            />
+          </div>
+          <WorkCardAvailabilityRow materialTypes={work.materialTypes} />
         </div>
-        <WorkCardAvailabilityRow materialTypes={work.materialTypes} />
-      </div>
+      </Link>
       <p className="mt-1 text-typo-subtitle-sm md:mt-5">{work.titles.full[0]}</p>
       <p className="mt-1 text-typo-caption opacity-50 md:mt-2">
         {displayCreators(work.creators, 2)}
       </p>
-    </Link>
+    </div>
   )
 }
 
