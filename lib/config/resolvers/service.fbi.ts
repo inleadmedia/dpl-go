87f04052444d3e729d1f7746a7c15d9109b91238
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-10-31 08:51:39 +0100

    Remove duplicate config func

--- lib/config/resolvers/service.fbi.ts
+++ lib/config/resolvers/service.fbi.ts
@@ -4,10 +4,6 @@ const serviceFbi = {
       return process.env.NEXT_PUBLIC_GRAPHQL_SCHEMA_ENDPOINT_FBI
     }
   },
-  // TODO: This is a temporary solution. We need to fetch the token via a graphql request.
-  "token.adgangsplatformen.library": () => {
-    return process.env.NEXT_PUBLIC_LIBRARY_TOKEN
-  },
 }
 
 export default serviceFbi
