dc855f21da0699a2da1e2cc216715077bd0b88b8
Adam Antal <adoantal@gmail.com>
2024-11-04 11:22:42 +0100

    Enable dynamic SVG imports in Icon component

--- components/shared/icon/Icon.tsx
+++ components/shared/icon/Icon.tsx
@@ -7,6 +7,7 @@ export default function Icon({ name, className }: IconProps) {
   try {
     if (!name) return null
     // TODO: Find a way to import SVGs dynamically
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
     const SVG = require(`../../../public/icons/${name}.svg`)?.default
     if (!SVG) return null
     return <SVG className={className} />
