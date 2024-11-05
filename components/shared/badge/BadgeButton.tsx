4770471761d85dfdcba201f214ab7de97172de92
Adam Antal <adoantal@gmail.com>
2024-11-05 15:06:17 +0100

    Add reusable BadgeButton component to not repeat code

---
+++ components/shared/badge/BadgeButton.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+import { cn } from "@/lib/helpers/helper.cn"
+
+type BadgeButtonProps = {
+  onClick: () => void
+  isActive?: boolean
+  classNames?: string
+  children: React.ReactNode
+}
+
+const BadgeButton = ({ onClick, isActive = false, classNames, children }: BadgeButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn([
+        `focus-visible h-[29px] w-auto self-start whitespace-nowrap rounded-full bg-background-overlay px-4
+        py-2 text-typo-caption hover:animate-wiggle`,
+        isActive && "bg-foreground text-background",
+        classNames,
+      ])}>
+      {children}
+    </button>
+  )
+}
+
+export default BadgeButton
