68e722570a2263cb4f8c78240665a39ec064d6da
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-11-07 11:32:29 +0100

    Splitting up machine in machine and setup
    And adding initial load more functionality

---
+++ lib/machines/search/search.machine.setup.ts
@@ -0,0 +1,101 @@
+import { assign, emit, setup } from "xstate"
+
+import { getFacets, performSearch } from "./queries"
+import { TContext, TFilters, TInput } from "./types"
+
+export default setup({
+  types: {
+    context: {} as TContext,
+    input: {} as TInput,
+  },
+  actions: {
+    toggleFilterInContext: assign({
+      selectedFilters: ({ event: { name, value }, context: { selectedFilters } }) => {
+        const filterName = name as keyof TFilters
+        if (!selectedFilters) {
+          selectedFilters = {}
+        }
+
+        if (!selectedFilters.hasOwnProperty(filterName)) {
+          selectedFilters[filterName] = []
+        }
+        // Remove filter.
+        if (selectedFilters[filterName] && selectedFilters[filterName].includes(value)) {
+          return {
+            ...selectedFilters,
+            [name]: selectedFilters[filterName].filter(filterValue => filterValue !== value),
+          }
+        }
+        // Add filter.
+        return {
+          ...selectedFilters,
+          [name]: [...(selectedFilters[filterName] ?? []), value],
+        }
+      },
+    }),
+    emitFilterToggled: emit(({ event }) => ({
+      type: "filterToggled",
+      toggled: event,
+    })),
+    assignQToContext: assign({
+      currentQ: ({ event }) => event.q,
+    }),
+    resetFiltersIfNoQ: assign({
+      selectedFilters: ({ context }) => {
+        if (!context.currentQ) {
+          return {}
+        }
+        return context.selectedFilters
+      },
+    }),
+    resetFilters: assign(() => ({
+      selectedFilters: {},
+    })),
+    setFacetDataInContext: assign({
+      facetData: ({
+        event: {
+          output: {
+            search: { facets },
+          },
+        },
+      }) => facets,
+    }),
+    setSearchDataInContext: assign({
+      searchData: ({
+        event: {
+          output: { search },
+        },
+        context: { searchData },
+      }) => {
+        return {
+          hitcount: search.hitcount,
+          pages: [...(searchData?.pages ?? []), [...search.works]],
+        }
+      },
+    }),
+    setQueryClientInContext: assign({
+      queryClient: ({ event }) => event.queryClient,
+    }),
+    setInitialFiltersInContext: assign({
+      selectedFilters: ({ event }) => event.filters,
+    }),
+    setLoadMoreValuesInContext: assign({
+      selectedFilters: ({ event }) => event.filters,
+    }),
+  },
+  actors: {
+    performSearch,
+    getFacets,
+  },
+  guards: {
+    eventHasSearchString: ({ event }) => {
+      return Boolean(event.q && event.q.length > 0)
+    },
+    contextHasSearchString: ({ context }) => {
+      return Boolean(context.currentQ && context.currentQ.length > 0)
+    },
+    contextHasQueryClient: ({ context }) => {
+      return Boolean(context.queryClient)
+    },
+  },
+})
--- lib/machines/search/search.machine.ts
+++ lib/machines/search/search.machine.ts
@@ -1,92 +1,10 @@
 import { assign, emit, setup } from "xstate"
 
 import { getFacets, performSearch } from "./queries"
+import searchMachineSetup from "./search.machine.setup"
 import { TContext, TFilters, TInput } from "./types"
 
-export default setup({
-  types: {
-    context: {} as TContext,
-    input: {} as TInput,
-  },
-  actions: {
-    toggleFilterInContext: assign({
-      selectedFilters: ({ event: { name, value }, context: { selectedFilters } }) => {
-        const filterName = name as keyof TFilters
-
-        if (!selectedFilters[filterName]) {
-          selectedFilters[filterName] = []
-        }
-        // Remove filter.
-        if (selectedFilters[filterName].includes(value)) {
-          return {
-            ...selectedFilters,
-            [name]: selectedFilters[filterName].filter(filterValue => filterValue !== value),
-          }
-        }
-        // Add filter.
-        return {
-          ...selectedFilters,
-          [name]: [...selectedFilters[filterName], value],
-        }
-      },
-    }),
-    emitFilterToggeled: emit(({ event }) => ({
-      type: "filterToggled",
-      toggled: event,
-    })),
-    assignQToContext: assign({
-      currentQ: ({ event }) => event.q,
-    }),
-    resetFiltersIfNoQ: assign({
-      selectedFilters: ({ context }) => {
-        if (!context.currentQ) {
-          return {}
-        }
-        return context.selectedFilters
-      },
-    }),
-    resetFilters: assign(() => ({
-      selectedFilters: {},
-    })),
-    setFacetDataInContext: assign({
-      facetData: ({
-        event: {
-          output: {
-            search: { facets },
-          },
-        },
-      }) => facets,
-    }),
-    setSearchDataInContext: assign({
-      searchData: ({
-        event: {
-          output: { search },
-        },
-      }) => search,
-    }),
-    setQueryClientInContext: assign({
-      queryClient: ({ event }) => event.queryClient,
-    }),
-    setInitialFiltersInContext: assign({
-      selectedFilters: ({ event }) => event.filters,
-    }),
-  },
-  actors: {
-    performSearch,
-    getFacets,
-  },
-  guards: {
-    eventHasSearchString: ({ event }) => {
-      return Boolean(event.q && event.q.length > 0)
-    },
-    contextHasSearchString: ({ context }) => {
-      return Boolean(context.currentQ && context.currentQ.length > 0)
-    },
-    contextHasQueryClient: ({ context }) => {
-      return Boolean(context.queryClient)
-    },
-  },
-}).createMachine({
+export default searchMachineSetup.createMachine({
   /** @xstate-layout N4IgpgJg5mDOIC5SzAQwE4GMAWA6ARgPaEAusJ6qADgMQDKAogCoD6AigKoMBKAmiwGEAMgEkGAOSYBtAAwBdRKCqFYASxKrCAO0UgAHogC0ANgAsATlwB2AMw2ArAA4Z5gEyOHN0wBoQATyN7G1xzGStzAEZbe1dI0NMAXwTfFAwcAmIyCmp6ZhZGAEFuAQAJfKZuEXEAcVkFJBBlNQ1tXQMEQ3sw63NzC3N7CONXe1MrXwCEe0sZRwGbPrjTYxkbJJS0LDwiUnJKWkZWKpEmEQKhFgAxESEmHjo63Sb1TR0G9pNXGVwY43tbGSmCIRGTDCaIKKWCKxaFfGyOCKmRymRLJECpLa4VQQAA2YBoTAA8tVqkIGFcbnduI8Gs8Wm9QO1XKZXLhnK5jFYvs4Yi57OCEKZQSFXHYbFYEf8kcZ1ujNulsXiCcTSeTrrceFIIvUlCoXq13ogOfZcLDZrYrIixj5-Ih7PZjGyEeYubFxdNHLKMQrcfimLwAApVWryJ56+ltQLGRymiLwr7TGxmcwCqymYIyEEomyuZlzL3yvCK-GFYolGm65qvSMIVxWb6mJxuYxuUZWCUC6NWXDLcxmKzRwEOVEbNJF325IqlLU6xrh6uG2tuawSmxRLMrOsCxyuCK4eFeeFA0HuewFse4ABmqhxJDA6FUWigBS0EDohcfUBoEG0YFw5FQO9-0LK8bzvB8nxfN8PyfCs5yrA1GSMCJ7SdBE7F6ONGx3AVgVZCJHE3SJ+iGAjz0xa9b3vT8oPfMdP2Ai9vWwT9v1-LEtAAN0IABrP9mNAqiIOfV86K2BiBOYz8EEfbjMEA146jgukFyQhAQQdEI+xcd0BTzEV-gREZyPSSjwJo0SYKgRjMSkp8aHvdBCHQXAqBxQDL2cgBbGzTLA6jIMs+in18vA7KgGSuMIeT6SU0NaXnRD9EQZFu0tRxbGtaEQRTW11L6EIZFFdMrDPNEBLMgKROg4LrMqlz6tYn8tD-WTeP4kDGsCmrxJC+rBPMp9IrkhTtDi2cVKS9oQQ5HsRglaF+Ty-5vjmE8ytHCj-OE2irIG+99uEhz0Ccly3I87zQsOiyepwBj+q6iK2pixT5GUxKGWShBUp7RF1oFQZHUI9LSqSNEtEICA4F0ZiwwQz6Pgy0wRURFCYkiDkbAFQxmT3FlzBsUFkS8aURzlC8diyfY4f1BGjB3R03FR+0YUx7GUOCNwCbCQd02MHMTPHPEaYjRdDGhJ0HWMIZG05Dxo07YVxQ8QZpkBFwyYq7abrEu6nxF1SvotXBRUBLslsmCJLFCUVsq6YZpYywXru63WWJC2GEvhmsJRNIJVeGC2IW6IVirre0zAWcxnce3baqu8KDamlK7B+BwUMDvSxh7IrD1B8rOu1129oE5qwCTun1LrGNbA8YEB1Kha9JbXA-mK90Y6L6q3fu7aK59xwTVN5YMqDwVYn3Tld2loZCf+KxO6EnW9oerv+8XDxgmH82AZbpMQY28mtqX4v4-6sv17U6EhR+OYkY5RuEQByJW-XUewYSIA */
   id: "search",
   initial: "bootstrap",
@@ -118,7 +36,7 @@ export default setup({
         TOGGLE_FILTER: [
           {
             guard: "contextHasSearchString",
-            actions: ["toggleFilterInContext", "emitFilterToggeled"],
+            actions: ["toggleFilterInContext", "emitFilterToggled"],
             target: "filteringAndSearching",
           },
           {
@@ -139,6 +57,11 @@ export default setup({
             target: "idle",
           },
         ],
+        LOAD_MORE: {
+          guard: "contextHasSearchString",
+          actions: ["setLoadMoreValuesInContext"],
+          target: "loadingMoreSearchResults",
+        },
       },
     },
     filteringAndSearching: {
@@ -205,6 +128,32 @@ export default setup({
       },
       onDone: {
         target: "#search.idle",
+      },
+    },
+    loadingMoreSearchResults: {
+      initial: "searching",
+      guard: "contextHasQueryClient",
+      states: {
+        searching: {
+          invoke: {
+            src: "performSearch",
+            input: ({ context }) => {
+              if (!context.queryClient) {
+                throw new Error("QueryClient is not set in context.")
+              }
+              return {
+                q: context.currentQ,
+                queryClient: context.queryClient,
+                filters: context.selectedFilters,
+              }
+            },
+            onDone: {
+              actions: ["setSearchDataInContext"],
+              target: "#search.idle",
+            },
+            onError: {},
+          },
+        },
       },
     },
   },
