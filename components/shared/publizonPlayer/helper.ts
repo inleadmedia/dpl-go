32cf0a3e133b87929624f8275bed05e46755db1b
thomasgross <thomasgrossrasmussen@gmail.com>
2024-11-25 16:36:57 +0100

    refactor: rename AssetType to TAssetType for consistency in asset management

--- components/shared/publizonPlayer/helper.ts
+++ components/shared/publizonPlayer/helper.ts
@@ -1,16 +1,16 @@
-type AssetType = {
+type TAssetType = {
   src: string
   type: "script" | "link"
 }
 
-export const assets: AssetType[] = [
+export const assets: TAssetType[] = [
   {
     src: "https://play.pubhub.dk/1.3.0/js/player-kernel.min.js",
     type: "script",
   },
 ]
 
-export const appendAsset = ({ src, type }: AssetType) => {
+export const appendAsset = ({ src, type }: TAssetType) => {
   if (type === "script") {
     const scriptElement = document.createElement("script")
     scriptElement.src = src
@@ -21,7 +21,7 @@ export const appendAsset = ({ src, type }: AssetType) => {
   }
 }
 
-export const removeAsset = ({ src, type }: AssetType) => {
+export const removeAsset = ({ src, type }: TAssetType) => {
   if (type === "script") {
     const scriptElement = document.querySelector(`script[src="${src}"]`)
     scriptElement?.remove()
