45b570822a694a3547f28b179b2ecad91f02db28
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-11-27 18:51:16 +0100

    Translating facet response to input search filters
    For some reason they are diffrent. Eg. lix => lixRange

--- lib/machines/search/search.machine.ts
+++ lib/machines/search/search.machine.ts
@@ -1,9 +1,10 @@
 import { and, not } from "xstate"
 
+import { getFiltersForSearchRequest } from "./helpers"
 import searchMachineSetup from "./search.machine.setup"
 
 export default searchMachineSetup.createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5SzAQwE4GMAWA6ARgPaEAusJ6qADgMQDKAogCoD6AigKoMBKAmiwGEAMgEkGAOSYBtAAwBdRKCqFYASxKrCAO0UgAHogC0ANgCMAVlwAWAJzGATDIAcNmeYDM9zwBoQAT0QLAHZcYycnIKdTSNcndysAXwTfFAwcAmIyCmp6ZhZGAEFuAQAJfKZuEXEAcVkFJBBlNQ1tXQMEQytjS0cbKyCg0xdwpytzXwCEd2jcGxt3d1Gne273AaSUtCw8IlJySlpGViqRJhECoRYAMREhJh46Ot0m9U0dBvaTR1Cw0ysFpzmLxuCaIKxDXCmdw2cwyFamYyLKIbECpbYZPbZWgAIQA8rimHQKgUAAosAAiuPEDCeDReLXeoE+5iiuCC0Js9isK36phhoIQphkNlC4Ui0RczniphRaPSuyyBxoeIJRO4pIpVJppnqShUr1aHyMLNMbI5XJ5g35-kQZlFESiMSl4NlW3SqggABswDQmLjqtUhAxrrd7txaXrmm82mD7CL2V4FjZwt0rFYBfZ7E5cB5hXzPOYhdN3K60ngPd7ff7A8GbnceFIdc99QyYwh7EERV4+stnEEgd0BYXLO5jEEuUEwp2zMZS+iKz6mLwSVVavJm1HDUzjdzZn84-NoYWxwLO1ZIcCZHCx+ZwbPkqi3eWvYvl6vG7rGi3o0bBUEZKEfwAu4t6eAi7gClC56jOYnaDCBQRpqOc7ui+uRFKUEZfpujL6IgizuDmrgODInj2KYfJxAKiIisefQyIMTg0TKD5ys+laFMUJQfhuBq4e0awhPuNgIrBhYxMYAqxLgEQwuyLieCBLGbGWuALjQQi4gU5IsAAsri3A0uudLflueGCiJuBxrY8SZlYV6FvYkF8iKTErOyQxeHBKF4AAZqonokGA6CqFoUAFFoEB0E+oVQDQEDaGAuDkKgQXJU+uD+YFwWxRFUUxWFWH0j+24dHyAHmJVslMW4fwyOmNoILeuBuOOYyAlyDhdD5mUBUFIVhXl0VlrF6WqWxsXxYlalaAAboQADWSVsb12UDeFkXDdso0rRNYUIKF82YKlbx1EVpn8YEbjGLMdjCms4yNVy2acrBUT2OYPVZf1uWbQVUBjeie1xcF6CEOguBUJ6qW+eDAC2gPpN9OWDX9I1hYjeDAwdc2EMdDJncZkZ8W2owhIx7L9OC5HCpBfSzHC8QPV9fUoxt+XowDyMQ9zk0JVoSWHYty0ZbzqMc9tGPc6tP37UL+OnfI504W2QorNYH3ih9Q7-jJxEfSza2-RLOCjdLYsg+gYMQ1DMPw5jMts0N-2OzzrPrTjR0ndohOfsVZntGT1h1Q4j2TMeMljkM-Y9Z6hCoBAsW6eDYBbTg3BwAAroFsCY3z01C0tDtxwnScp2n2AZ7A2dkHncu4wrPtK0T2Ek7+hh8jdXT1cY-TmIe5FOFJ3wfbeLlhB9Yyx-Hidhcn6Cp0+Vc17nwM0KD4OQ9DJCw+gCMrSXs9QPPi9lsvOd11Ant497Wi+7xrbt6YWYXu9DEdh4aZh4EiZWV05F-DiH8CwSQHxaEIBAOAug2IPxKuZQwgJCIRA7KOScvJrSTA7lyM0sEiwyDCB4FwPUFT7GoLAgORgnBuBkuOdkY5e5Wm-h0NYVkEL1W5OYUOMJEisQyguchl0yr2BkoCYwM4xhjjiGEai+DcBrDiMOVw9k+iG1luzCusUBFtnZCEYEXQIhML5AzRMlF+xpgoveFS6ILbO05pjLRv5IiWA8MOUOkEGLWEZvIqc443CqKdmjSWANdr-QcaVOIhEXEWDcU9fonivD-Bjrw1SNjAmmwxitfmYAwnmWfpEM0QDog3nFBmYwIpugJOZsk6x7tjYaKlu7HJ7QnGsJ7gYgU3IRSoISbCOM+DlKPhSbU8W9Subu1dpokyKtfwRNafo-sQ4ylyKjgY-x61bFBNdrgLJTSf72RzMMRC7k3qmCHJZMwFNPrVPSIfMuC8K7nzILsjotgRRQn7gsTM-Z4hjGcmMNk-9royD5O86epc57lyXlnC+wNnmdELJCECA8P4-MMb-TqACrBAPBFcpIQA */
+  /** @xstate-layout N4IgpgJg5mDOIC5SzAQwE4GMAWA6ARgPaEAusJ6qADgMQDKAogCoD6AigKoMBKAmiwGEAMgEkGAOSYBtAAwBdRKCqFYASxKrCAO0UgAHogC0ANgCMAVlwAWAJzGATDIAcNmeYDM9zwBoQAT0QLAHZcYycnIKdTSNcndysAXwTfFAwcAmIyCmp6ZhZGAEFuAQAJfKZuEXEAcVkFJBBlNQ1tXQMEQytjS0cbKyCg0xdwpytzXwCEd2jcGxt3d1Gne273AaSUtCw8IlJySlpGViqRJhECoRYAMREhJh46Ot0m9U0dBvaTR1Cw0ysFpzmLxuCaIKxDXCmdw2cwyFamYyLKIbECpbYZPbZWgAIQA8rimHQKgUAAosAAiuPEDCeDReLXeoE+5iiuCC0Js9isK36phhoIQphkNlC4Ui0RczniphRaPSuyyBxoeIJRO4pIpVJppnqShUr1aHyMLNMbI5XJ5g35-kQZlFESiMSl4NlW3SqggABswDQmLjqtUhAxrrd7txaXrmm82oh4j17C47OYHE5jALHPZcOYuiNxXF3A5XWk8B7vb7-YHgzc7jwpDrnvqGTGEPYgiKvH1ls4gkDugLzBZcAWglygmE22ZjEX0aWfUxeCSqrV5A2o4amcbubM-vY5gsYQiggK21ZIcCZHDjD3wVPkqi3SWvT7CsUShHGo3o0apnEs64HDInj2KYfJxAKiIigOxh9DIgyptCMp3nKj5li+pR1rqH5roy+ixgMkLcjYCLmD2Ep2AKsS4BEMLsi4njuAO07uk+NBCLiBTkiwACyuLcDSK50p+664YKRG4LutjxPYXIXgO9gCiBcxUQ4V7TMsaw2EETF4AAZqonokGA6CqFoUAFFoEB0A+JlQDQEDaGAuDkKghlOQ+uB6QZRk2eZlnWaZ770l+G4dHyMhZuYLJtqmbh-DIVj9qebgjmMgJcg4XTaR5+mGcZpm+VZxY2W5xYldsNl2Q5uAmQAboQADWjnIdlXl5WZFmFeVpllekyE2QgtWEJgLlvHUgVCTh7RCsmsx2MKazjDaLZWE4sz2CRUTrVlnm5T5HX+VAPV4H1pk0EZ6CEOguBUJ6Lk6ZdAC2R0tbt+X7UV3XNSdUADVodXDQyY0CZGBqTYgowhHB7L9OCwHCgpfSzHC8QLdtOXeW9fkfYdO1GS9GO2fZWiOYNjXPbjbUFQd+NXRT-WDQDo3yON2HNkKKzWOt4rrf2QThZKDjmGjrV7VjXU4+jtOSxV52Xddt0kPd6BPc1dOY51ODFRTNP039Q0jdoQOYUFwntBD1hxYL-YIspUNC0h7meoQqAQDZXGXWAGvYNwcAAK4GbAR0VUTJN62TzVOy7bse17PuwP7ZBB6Zv3-QbWhG6uoPNoYfLGNYxjxcY-TmPMnhDBR3zrdmfJ2Ms2b25spWR67pnu+gnsPnHCeB99Z3oBdV03Xdj3Pc30ft7HfsB0nP0M2nGeCaz345wmZ6bbBrYeFYYwKV47jiV0wF-HEfwWEkd5aIQEBwLoyGZ02y+AvvEStsORdWotkwr6e7LJpyXbQSFDYLKCp9jUHvsFEShgnBuCoiOdkV5358k-kYNY4kGJ823utQWMJEgO1KrOCBpsjCmEzOEZMk4xhXjiGEcCMg85rDiAOEu8VhR4MbuiNW7Uxaa1MkQsGCB2QhGBDmHsCMkZeC8DA2EJFuzC1etwr2xU76Lyzt+SIlgPDMKtktIUIQrDI0igxSSK0G73lKlwqm2NnrfX4c2fMWYGIWB0ZMUc1hkb-B7PIgmVjxbPRDnY78pDIhmhPtEK8G1TDpmgqEIEKMvH4M4dLdW1MKaBJCho9BhcIgoIQIRIcV4LTrU0jDW8HD0iWPen47WXD0kiQcSI8cuS-4FLtt4ymVTeES1argAJqiH4hVIQYrMwwgjcgieKfsYkzBtMSekMercY6dynmQOpnxbAiihCXBY0kezxB3rov4lgxkqQygmFwWUFlQDbh3YsXdp62P6ZA9ZA5IQMVLrsreBzJikIWAfBwIEVrTHBPbJIQA */
   id: "search",
   initial: "bootstrap",
   context: ({ input }) => ({
@@ -90,10 +91,11 @@ export default searchMachineSetup.createMachine({
                   if (!context.queryClient) {
                     throw new Error("QueryClient is not set in context.")
                   }
+
                   return {
                     q: context.currentQuery,
                     queryClient: context.queryClient,
-                    filters: context.selectedFilters,
+                    filters: getFiltersForSearchRequest(context),
                     offset: context.searchOffset,
                     limit: context.searchPageSize,
                   }
@@ -123,7 +125,7 @@ export default searchMachineSetup.createMachine({
                   return {
                     q: context.currentQuery,
                     queryClient: context.queryClient,
-                    filters: context.selectedFilters,
+                    filters: getFiltersForSearchRequest(context),
                     facetLimit: context.facetLimit,
                   }
                 },
@@ -158,7 +160,7 @@ export default searchMachineSetup.createMachine({
               return {
                 q: context.currentQuery,
                 queryClient: context.queryClient,
-                filters: context.selectedFilters,
+                filters: getFiltersForSearchRequest(context),
                 offset: context.searchOffset,
                 limit: context.searchPageSize,
               }
