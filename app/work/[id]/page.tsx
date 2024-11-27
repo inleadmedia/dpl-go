4d23f0f6da757495592efbf724505a2cf9eb7001
Adam Antal <adoantal@gmail.com>
2024-11-27 22:26:20 +0100

    Refactor WorkPage components to support dehydrated query data and improve data fetching

--- app/work/[id]/page.tsx
+++ app/work/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from "react"
 
 import WorkPageLayout from "@/components/pages/workPageLayout/WorkPageLayout"
 import getQueryClient from "@/lib/getQueryClient"
-import { useGetMaterialQuery } from "@/lib/graphql/generated/fbi/graphql"
+import { GetMaterialQuery, useGetMaterialQuery } from "@/lib/graphql/generated/fbi/graphql"
 
 type PageProps = {
   params: {
@@ -11,19 +11,31 @@ type PageProps = {
   }
 }
 
-function Page({ params: { id } }: PageProps) {
+async function Page({ params: { id } }: PageProps) {
   const queryClient = getQueryClient()
-
   const decodedWid = decodeURIComponent(id)
 
-  queryClient.prefetchQuery({
+  // Wait for the query to finish fetching before dehydrating
+  await queryClient.prefetchQuery({
     queryKey: useGetMaterialQuery.getKey({ wid: decodedWid }),
     queryFn: useGetMaterialQuery.fetcher({ wid: decodedWid }),
   })
 
+  // Dehydrate the query data after ensuring it is fetched
+  const dehydratedState = dehydrate(queryClient)
+
+  // Get the preloaded data from the query client
+  const dehydratedQueryData: GetMaterialQuery | undefined = queryClient.getQueryData(
+    useGetMaterialQuery.getKey({ wid: decodedWid })
+  )
+
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <WorkPageLayout workId={decodedWid} />
+    <HydrationBoundary state={dehydratedState}>
+      <WorkPageLayout
+        key={decodedWid}
+        workId={decodedWid}
+        dehydratedQueryData={dehydratedQueryData}
+      />
     </HydrationBoundary>
   )
 }
--- components/pages/workPageLayout/WorkPageLayout.tsx
+++ components/pages/workPageLayout/WorkPageLayout.tsx
@@ -3,7 +3,7 @@
 import { useQuery } from "@tanstack/react-query"
 import React, { useEffect } from "react"
 
-import { useGetMaterialQuery } from "@/lib/graphql/generated/fbi/graphql"
+import { GetMaterialQuery, useGetMaterialQuery } from "@/lib/graphql/generated/fbi/graphql"
 import { useSelectedManifestationStore } from "@/store/selectedManifestation.store"
 
 import WorkPageHeader from "./WorkPageHeader"
@@ -11,12 +11,14 @@ import { getBestRepresentation } from "./helper"
 
 type WorkPageLayoutProps = {
   workId: string
+  dehydratedQueryData: GetMaterialQuery | undefined
 }
 
-function WorkPageLayout({ workId }: WorkPageLayoutProps) {
+function WorkPageLayout({ workId, dehydratedQueryData }: WorkPageLayoutProps) {
   const { data, isLoading } = useQuery({
     queryKey: useGetMaterialQuery.getKey({ wid: workId }),
     queryFn: useGetMaterialQuery.fetcher({ wid: workId }),
+    initialData: dehydratedQueryData,
   })
   const pageContainerClasses =
     "content-container my-grid-gap-2 flex-row flex-wrap space-y-grid-gap-2 lg:space-y-grid-gap-1 lg:my-grid-gap-half"
