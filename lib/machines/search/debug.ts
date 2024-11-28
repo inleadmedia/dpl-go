71b70c4527a879d57b94d14fb43d19f59d42f2ab
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-11-28 09:33:20 +0100

    Allow console when deliberately debugging

--- lib/machines/search/debug.ts
+++ lib/machines/search/debug.ts
@@ -2,6 +2,7 @@ import { InspectionEvent } from "xstate"
 
 export const debugEvents = (inspectionEvent: InspectionEvent) => {
   if (inspectionEvent.type === "@xstate.event") {
+    // eslint-disable-next-line no-console
     console.debug(inspectionEvent.event)
   }
 }
