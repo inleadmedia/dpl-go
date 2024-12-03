b647113728cdc07f75cb6e71325372687e8dd8c3
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-12-03 21:46:41 +0100

    Moving lagoon url environment handling to `yarn start` level
    Seems like the only place where we can access the Lagoon env variables
    is via a script in the node container.
    We probably have to do this differently with production domains, since
    we are now matching urls called something with: "nginx", "varnish" and
    such.

---
+++ lagoon/set-env-variables.sh
@@ -0,0 +1,29 @@
+
+getLagoonUrl() {
+    local type=$1
+    IFS=',' read -r -a routes <<< "$LAGOON_ROUTES"
+    for route in "${routes[@]}"; do
+      if [[ $route == *"$type"* ]]; then
+        echo "$route"
+        return
+      fi
+    done
+  }
+
+# Make sure app.url is set in application
+app_url=$(getLagoonUrl node)
+if [ -z "$app_url" ]; then
+  echo "Error: Unable to determine app URL"
+  exit 1
+fi
+
+# Make sure the DPL CMS graphql schema endpoint is set in application
+cms_url=$(getLagoonUrl nginx)
+if [ -z "$cms_url" ]; then
+  echo "Error: Unable to determine CMS URL"
+  exit 1
+fi
+
+
+export NEXT_PUBLIC_APP_URL="$app_url"
+export NEXT_PUBLIC_GRAPHQL_SCHEMA_ENDPOINT_DPL_CMS="$cms_url/graphql"
