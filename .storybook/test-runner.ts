d1a852d839fc9f598b14bae3dac74469e8236c11
Adam Antal <adoantal@gmail.com>
2024-12-10 15:49:59 +0100

    Add test delays to ensure animations complete before accessibility checks

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -3,8 +3,13 @@ import { checkA11y, injectAxe } from "axe-playwright"
 module.exports = {
   async preVisit(page) {
     await injectAxe(page)
+    // Add a delay to wait for all animations to finish
+    await page.waitForTimeout(2000)
   },
   async postVisit(page) {
+    // Ensure all animations are complete before running accessibility checks
+    await page.waitForTimeout(2000)
+
     await checkA11y(page, "#storybook-root", {
       axeOptions: {},
       detailedReport: true,
