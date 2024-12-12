1cb6365266a630bdc5d89d23d0652cb4010f27d9
Adam Antal <adoantal@gmail.com>
2024-12-12 12:38:55 +0100

    Add InfoBoxItem component for displaying terms and descriptions with optional buttons

---
+++ components/shared/infoBox/InfoBoxItem.tsx
@@ -0,0 +1,43 @@
+import { useRouter } from "next/navigation"
+import React from "react"
+
+import useSearchMachineActor from "@/lib/machines/search/useSearchMachineActor"
+
+import { Button } from "../button/Button"
+
+type InfoBoxItemProps = {
+  term: string
+  description: string[]
+  isButtons?: boolean
+}
+
+const InfoBoxItem = ({ term, description, isButtons = false }: InfoBoxItemProps) => {
+  const router = useRouter()
+  const actor = useSearchMachineActor()
+
+  const handleClick = (text: string) => {
+    actor.send({ type: "SEARCH" })
+    router.push(`/search?q=${text}`, {
+      scroll: false,
+    })
+  }
+
+  return (
+    <>
+      <hr className="not-first:mt-7 border-foreground opacity-15" />
+      <dt className="mt-2 text-typo-caption">{term}</dt>
+      {!isButtons && <dd className="mt-6 text-typo-heading-5">{description.join(", ")}</dd>}
+      {isButtons && (
+        <div className="mt-6 flex flex-row flex-wrap gap-2">
+          {description.map(desc => (
+            <Button key={desc} size={"sm"} className="px-3" onClick={() => handleClick(desc)}>
+              {desc}
+            </Button>
+          ))}
+        </div>
+      )}
+    </>
+  )
+}
+
+export default InfoBoxItem
