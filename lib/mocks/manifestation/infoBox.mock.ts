7d32fdce471257699b3a4494ee223a4eab0b0011
Adam Antal <adoantal@gmail.com>
2024-12-18 10:20:05 +0100

    Add mock data for manifestation in infoBox.mock.ts

---
+++ lib/mocks/manifestation/infoBox.mock.ts
@@ -0,0 +1,88 @@
+import { ManifestationWorkPageFragment } from "@/lib/graphql/generated/fbi/graphql"
+
+const manifestationMock = {
+  pid: "870970-basis:51579623",
+  identifiers: [
+    {
+      type: "PUBLIZON",
+      value: "9788762722880",
+    },
+    {
+      type: "ISBN",
+      value: "9788762722880",
+    },
+  ],
+  materialTypes: [
+    {
+      materialTypeGeneral: {
+        display: "e-bøger",
+        code: "EBOOKS",
+      },
+    },
+  ],
+  access: [
+    {
+      __typename: "Ereol",
+      origin: "eReolen",
+      url: "https://ereolen.dk/ting/object/870970-basis:51579623",
+      canAlwaysBeLoaned: false,
+    },
+    {
+      __typename: "Ereol",
+      origin: "eReolen Go",
+      url: "https://ereolengo.dk/ting/object/870970-basis:51579623",
+      canAlwaysBeLoaned: false,
+    },
+  ],
+  titles: {
+    identifyingAddition: null,
+    full: ["Den vingede hest Skar"],
+  },
+  languages: {
+    main: [
+      {
+        display: "dansk",
+        isoCode: "dan",
+      },
+    ],
+  },
+  audience: {
+    ages: [
+      {
+        display: "9-11",
+      },
+    ],
+  },
+  series: [
+    {
+      numberInSeries: "Del 14",
+      title: "Monsterjagten",
+    },
+  ],
+  subjects: {
+    all: [
+      {
+        display: "monstre",
+      },
+    ],
+  },
+  physicalDescription: {
+    summaryFull: "112 sider, ill.",
+  },
+  dateFirstEdition: {
+    display: "2015",
+  },
+  genreAndForm: ["roman", "fantasy"],
+  publisher: ["Flachs"],
+  contributors: [
+    {
+      display: "Trine Bech",
+    },
+    {
+      display: "Steve Sims",
+    },
+  ],
+  contributorsFromDescription: [],
+} as ManifestationWorkPageFragment
+
+export default manifestationMock
