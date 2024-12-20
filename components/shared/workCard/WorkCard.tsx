31a3cbc018d17aeb0fa3b3bbd9a70ca18dfe62d0
Adam Antal <adoantal@gmail.com>
2024-12-20 10:47:24 +0100

    Move displayCreators helper into a global helper file as it's used globally

--- components/shared/workCard/WorkCard.tsx
+++ components/shared/workCard/WorkCard.tsx
@@ -3,6 +3,7 @@ import React from "react"
 
 import { WorkTeaserSearchPageFragment } from "@/lib/graphql/generated/fbi/graphql"
 import { getCoverUrls, getLowResCoverUrl } from "@/lib/helpers/covers"
+import { displayCreators } from "@/lib/helpers/helper.creators"
 import { resolveUrl } from "@/lib/helpers/helper.routes"
 import { getIsbnsFromWork } from "@/lib/helpers/ids"
 import { useGetCoverCollection } from "@/lib/rest/cover-service-api/generated/cover-service"
@@ -12,7 +13,7 @@ import { useGetV1ProductsIdentifier } from "@/lib/rest/publizon-api/generated/pu
 import { Badge } from "../badge/Badge"
 import { CoverPicture } from "../coverPicture/CoverPicture"
 import WorkCardAvailabilityRow from "./WorkCardAvailabilityRow"
-import { displayCreators, getAllWorkPids } from "./helper"
+import { getAllWorkPids } from "./helper"
 
 type WorkCardProps = {
   work: WorkTeaserSearchPageFragment
---
+++ lib/helpers/helper.creators.ts
@@ -0,0 +1,21 @@
+import {
+  ManifestationDetailsFragment,
+  WorkFullWorkPageFragment,
+} from "@/lib/graphql/generated/fbi/graphql"
+
+export const displayCreators = (
+  creators: WorkFullWorkPageFragment["creators"] | ManifestationDetailsFragment["contributors"],
+  amount: number
+) => {
+  return creators.reduce((acc, creator, index) => {
+    // We shorten to max <amount> creators
+    if (index === amount) {
+      return acc + ", et. al"
+    }
+    // We don't want to show one person twice
+    if (index > amount || acc.includes(creator.display)) {
+      return acc
+    }
+    return acc + (index > 0 ? ", " : "") + creator.display
+  }, "")
+}
