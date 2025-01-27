21c3381104c266d6e295c180bcb49db066ec8106
Adam Antal <adoantal@gmail.com>
2025-01-27 15:22:52 +0100

    Link to search on clicking on series name on work page

--- components/shared/infoBox/InfoBoxItem.tsx
+++ components/shared/infoBox/InfoBoxItem.tsx
@@ -3,13 +3,27 @@ import React from "react"
 
 import { Button } from "@/components/shared/button/Button"
 
+type isButtonsType = {
+  isButtons?: true
+  isSearchable?: false
+}
+
+type isSearchableType = {
+  isSearchable?: true
+  isButtons?: false
+}
+
 type InfoBoxItemProps = {
   term: string
   description: string[]
-  isButtons?: boolean
-}
+} & (isButtonsType | isSearchableType)
 
-const InfoBoxItem = ({ term, description, isButtons = false }: InfoBoxItemProps) => {
+const InfoBoxItem = ({
+  term,
+  description,
+  isButtons = false,
+  isSearchable = false,
+}: InfoBoxItemProps) => {
   const router = useRouter()
 
   const handleClick = (text: string) => {
@@ -23,7 +37,7 @@ const InfoBoxItem = ({ term, description, isButtons = false }: InfoBoxItemProps)
       <hr className="border-border not-first:mt-7" aria-hidden="true" />
       <dt className="mt-2 text-typo-caption">{term}</dt>
       {!description.length && <dd className="mt-6 text-typo-heading-5">-</dd>}
-      {!!description.length && !isButtons && (
+      {!!description.length && !isButtons && !isSearchable && (
         <dd className="mt-6 text-typo-heading-5">{description.join(", ")}</dd>
       )}
       {!!description.length && isButtons && (
@@ -32,6 +46,22 @@ const InfoBoxItem = ({ term, description, isButtons = false }: InfoBoxItemProps)
             <Button key={item} size={"sm"} className="px-3" onClick={() => handleClick(item)}>
               {item}
             </Button>
+          ))}
+        </dd>
+      )}
+      {isSearchable && (
+        <dd className="mt-6 text-typo-heading-5">
+          {description.map(item => (
+            <span key={item}>
+              {/* "Del xyz i " is not a link to search */}
+              <span>{item.match(/^Del \d+ i /)?.[0]}</span>
+              {/* Name of the series links to a search. */}
+              <span
+                onClick={() => handleClick(item.replace(/^Del \d+ i /, ""))}
+                className="animate-text-underline">
+                {item.replace(/^Del \d+ i /, "")}
+              </span>
+            </span>
           ))}
         </dd>
       )}
