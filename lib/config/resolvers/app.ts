5e82807dbc78c35bd4fea04e1c32e333d361c517
thomasgross <thomasgrossrasmussen@gmail.com>
2025-01-29 12:36:25 +0100

    fix: remove Chromatic URL condition from app configuration

--- lib/config/resolvers/app.ts
+++ lib/config/resolvers/app.ts
@@ -1,10 +1,5 @@
-import isChromatic from "chromatic/isChromatic"
-
 const app = {
   "app.url": () => {
-    if (isChromatic()) {
-      return "https://hellboy.the-movie.com"
-    }
     if (process.env.NEXT_PUBLIC_APP_URL) {
       return process.env.NEXT_PUBLIC_APP_URL
     }
