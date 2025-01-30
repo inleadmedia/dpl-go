1ef8403ab4551b236a52d185103f8fe2202119ba
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2025-01-30 11:01:05 +0100

    Comment why we are ensuring dpl cms data
    as a very early step

--- instrumentation.ts
+++ instrumentation.ts
@@ -4,5 +4,8 @@ import { ensureDplCmsConfig } from "./lib/config/dpl-cms/dplCmsConfig"
 
 export async function register() {
   const queryClient = new QueryClient({})
+  // By doing this, we ensure that the DPL CMS configuration is loaded
+  // before any other queries are executed.
+  // The next time we will call it server side it is cached.
   ensureDplCmsConfig(queryClient)
 }
