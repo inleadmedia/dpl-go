04e77ff16df23968b28c97ce61778b2872ca2c99
thomasgross <thomasgrossrasmussen@gmail.com>
2025-02-03 13:23:23 +0100

    refactor: simplify routes configuration by directly assigning slug

--- lib/config/resolvers/routes.ts
+++ lib/config/resolvers/routes.ts
@@ -1,9 +1,7 @@
 const GO_FRONT_PAGE_SLUG = "go-frontpage"
 
 const routes = {
-  "routes.gofrontpage": () => {
-    return GO_FRONT_PAGE_SLUG
-  },
+  "routes.gofrontpage": GO_FRONT_PAGE_SLUG,
 }
 
 export default routes
