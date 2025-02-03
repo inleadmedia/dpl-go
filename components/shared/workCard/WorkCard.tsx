2cf7a72094683306d2f29e956d49e3c421d45297
Adam Antal <adoantal@gmail.com>
2025-02-03 16:45:41 +0100

    Add stacked variant for WorkCard component

--- components/shared/workCard/WorkCard.tsx
+++ components/shared/workCard/WorkCard.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import React from "react"
 
 import { WorkTeaserSearchPageFragment } from "@/lib/graphql/generated/fbi/graphql"
+import { cn } from "@/lib/helpers/helper.cn"
 import { getCoverUrls, getLowResCoverUrl } from "@/lib/helpers/helper.covers"
 import { displayCreators } from "@/lib/helpers/helper.creators"
 import { resolveUrl } from "@/lib/helpers/helper.routes"
@@ -15,11 +16,25 @@ import { CoverPicture } from "../coverPicture/CoverPicture"
 import WorkCardAvailabilityRow from "./WorkCardAvailabilityRow"
 import { getAllWorkPids } from "./helper"
 
-type WorkCardProps = {
-  work: WorkTeaserSearchPageFragment
+type StackedCard = {
+  isStacked: true
+  orderNumber: number
+  zIndex: number
 }
 
-const WorkCard = ({ work }: WorkCardProps) => {
+type NonStackedCard = {
+  isStacked?: never
+  orderNumber?: never
+  zIndex?: never
+}
+
+type WorkCardProps = {
+  work: WorkTeaserSearchPageFragment
+  isStacked?: boolean
+  className?: string
+} & (StackedCard | NonStackedCard)
+
+const WorkCard = ({ work, isStacked, orderNumber, zIndex, className }: WorkCardProps) => {
   const { data: dataCovers, isLoading: isLoadingCovers } = useGetCoverCollection({
     type: "pid",
     identifiers: [getAllWorkPids(work).join(", ")],
@@ -54,12 +69,25 @@ const WorkCard = ({ work }: WorkCardProps) => {
 
   return (
     <Link
-      className="block space-y-3 lg:space-y-5"
-      href={resolveUrl({ routeParams: { work: "work", wid: work.workId } })}>
-      <div>
+      className={cn("block space-y-3 lg:space-y-5", className, {
+        "absolute left-0 top-0 h-full w-full": isStacked,
+      })}
+      href={resolveUrl({ routeParams: { work: "work", wid: work.workId } })}
+      aria-hidden={isStacked && orderNumber !== 0}
+      tabIndex={isStacked && orderNumber !== 0 ? -1 : 0}
+      style={isStacked ? { zIndex } : undefined}>
+      <div className={cn({ relative: isStacked })}>
         <div
           key={work.workId}
-          className="relative flex aspect-4/5 h-auto w-full flex-col rounded-base bg-background-overlay px-[15%] pt-[15%]">
+          className={cn(
+            "relative flex aspect-4/5 h-auto w-full flex-col rounded-base bg-background-overlay px-[15%] pt-[15%]",
+            {
+              "bg-background": isStacked,
+              "shadow-stacked-card": isStacked && orderNumber < 3,
+              "rotate-3": isStacked && orderNumber === 1,
+              "-rotate-3": isStacked && orderNumber === 2,
+            }
+          )}>
           {!!dataPublizon?.product?.costFree && (
             <Badge variant={"blue-title"} className="absolute left-4 top-4 md:left-4 md:top-4">
               BLÅ
@@ -71,7 +99,7 @@ const WorkCard = ({ work }: WorkCardProps) => {
                 lowResSrc={lowResCover || ""}
                 src={coverSrc?.[0] || ""}
                 alt={`${work.titles.full[0]} cover billede`}
-                withTilt
+                withTilt={!isStacked}
                 className="select-none"
               />
             )}
@@ -82,7 +110,7 @@ const WorkCard = ({ work }: WorkCardProps) => {
         </div>
       </div>
 
-      <div className="space-y-2">
+      <div className={cn("space-y-2", { hidden: isStacked && orderNumber !== 0 })}>
         <p className="mr-grid-column-half break-words text-typo-subtitle-lg">
           {work.titles.full[0]}
         </p>
