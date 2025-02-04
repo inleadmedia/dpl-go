c103b097b64bf0ad35b3c886fcf9849c385c980e
Adam Antal <adoantal@gmail.com>
2025-02-04 15:47:27 +0100

    Add VideoBundle to the ParagraphResolver

--- components/paragraphs/ParagraphResolver.tsx
+++ components/paragraphs/ParagraphResolver.tsx
@@ -2,12 +2,14 @@ import React from "react"
 
 import { ParagraphUnion } from "@/lib/graphql/generated/dpl-cms/graphql"
 
-import ParagraphGoVideo from "./ParagraphGoVideo/ParagraphGoVideo"
+import Video from "./Video/Video"
+import VideoBundle from "./VideoBundle/VideoBundle"
 import { ParagraphErrorBoundary } from "./paragraphErrorBoundary/paragraphErrorBoundary"
 
 function ParagraphResolver({ paragraphs }: { paragraphs: ParagraphUnion[] }) {
   const components = {
-    ParagraphGoVideo,
+    Video,
+    VideoBundle,
   }
 
   return paragraphs.map((paragraph, index) => {
