b3b2eb36818b88f167255cc4c1fd41f1be8fd436
Adam Antal <adoantal@gmail.com>
2025-02-07 15:17:09 +0100

    Update VideoBundle.stories.tsx to match new component props

--- components/paragraphs/VideoBundle/VideoBundle.stories.tsx
+++ components/paragraphs/VideoBundle/VideoBundle.stories.tsx
@@ -2,7 +2,8 @@ import type { Meta, StoryObj } from "@storybook/react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import { darkModeDecorator } from "@/.storybook/decorators"
-import VideoBundle, { VideoBundleMaterial } from "@/components/paragraphs/VideoBundle/VideoBundle"
+import VideoBundle, { VideoBundleProps } from "@/components/paragraphs/VideoBundle/VideoBundle"
+import { WorkId } from "@/lib/types/ids"
 
 const queryClient = new QueryClient()
 
@@ -16,14 +17,17 @@ const withQueryClient = (StoryComponent: any) => (
 
 const manualBundleArgs = {
   title: "Video Bundle - Manual",
-  url: "https://media.videotool.dk/?vn=557_2022121911105390173777808898",
-  materials: [
-    { materialType: "bog", workId: "work-of:870970-basis:52516749" },
-    { materialType: "e-bog", workId: "work-of:870970-basis:53559700" },
-    { materialType: "artikel", workId: "work-of:870971-tsart:82862382" },
-    { materialType: "artikel (online)", workId: "work-of:870971-avis:36769580" },
-  ] as VideoBundleMaterial[],
-}
+  embedVideo: {
+    mediaVideotool: "https://media.videotool.dk/?vn=557_2022121911105390173777808898",
+    name: "Video Title",
+  },
+  videoBundleWorkIds: [
+    "work-of:870970-basis:52516749",
+    "work-of:870970-basis:53559700",
+    "work-of:870971-tsart:82862382",
+    "work-of:870971-avis:36769580",
+  ] as WorkId[],
+} as VideoBundleProps
 
 const meta = {
   title: "components/VideoBundle",
@@ -31,10 +35,16 @@ const meta = {
   parameters: { layout: "centered" },
   args: {
     title: "Video Bundle - Automatic",
-    url: "https://media.videotool.dk/?vn=557_2022121911105390173777808898",
-    cqlSearchString:
-      "(term.title='adams' AND term.title= 'bold') OR (term.title= 'adams' AND term.title= 'ballon') OR (term.title= 'adams' AND term.title= 'pony') OR (term.title= 'adams' AND term.title= 'cykel') OR (term.title= 'adams' AND term.title= 'fugl')",
-    amountOfMaterials: 5,
+    embedVideo: {
+      mediaVideotool: "https://media.videotool.dk/?vn=557_2022121911105390173777808898",
+      name: "Video Title",
+    },
+    cqlSearch: {
+      __typename: "CQLSearch",
+      value:
+        "(term.title='adams' AND term.title= 'bold') OR (term.title= 'adams' AND term.title= 'ballon') OR (term.title= 'adams' AND term.title= 'pony') OR (term.title= 'adams' AND term.title= 'cykel') OR (term.title= 'adams' AND term.title= 'fugl')",
+    },
+    videoAmountOfMaterials: 5,
   },
   decorators: [withQueryClient],
 } satisfies Meta<typeof VideoBundle>
@@ -44,7 +54,7 @@ type Story = StoryObj<typeof meta>
 
 export const Automatic: Story = {
   render: args => (
-    <div className="my-grid-gap-2 h-full w-[100vw] space-y-grid-gap-2">
+    <div className="my-grid-gap-2 space-y-grid-gap-2 h-full w-[100vw]">
       <VideoBundle {...args} />
     </div>
   ),
