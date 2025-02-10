d56f8f8737ea0f272b7ecf28d8364c2fa88544cf
Adam Antal <adoantal@gmail.com>
2025-02-10 12:34:06 +0100

    Add skeleton screens to VideoBundle

--- components/paragraphs/VideoBundle/VideoBundle.tsx
+++ components/paragraphs/VideoBundle/VideoBundle.tsx
@@ -184,4 +184,29 @@ const VideoBundle = ({
   )
 }
 
+export const SearchFiltersDesktopSkeleton = () => {
+  return (
+    <div className="bg-background-skeleton">
+      <div className="content-container">
+        <div className="gap-paragraph-spacing-inner w-full py-4 text-center md:py-12 lg:py-16">
+          <div className="bg-background mr-auto mb-4 ml-auto block h-10 w-36 animate-pulse rounded-full md:mb-10 lg:w-72" />
+          <div className="flex w-full flex-col items-start gap-11 lg:flex-row lg:gap-0">
+            <div className="rounded-base bg-background relative aspect-16/9 w-full animate-pulse overflow-hidden lg:w-[75%]" />
+            <div
+              className="gap-grid-gap flex w-full flex-row flex-wrap items-center justify-center lg:w-[25%] lg:justify-end
+                lg:pl-4">
+              <div className="md:ml-grid-column-2 mr-auto h-[24px] w-[24px] rounded-full lg:hidden" />
+              <div
+                className="bg-background rounded-base relative aspect-4/9 h-[250px] w-[177px] animate-pulse md:aspect-3/5
+                  md:w-[300px] lg:h-[450px]"
+              />
+              <div className="md:mr-grid-column-2 ml-auto h-[24px] w-[24px] rounded-full lg:hidden" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export default VideoBundle
