8b876ca138dcb11a99544505c290614d4d70a846
Adam Antal <adoantal@gmail.com>
2025-02-12 13:06:06 +0100

    Add both material slider components into paragraph resolver

--- components/paragraphs/ParagraphResolver.tsx
+++ components/paragraphs/ParagraphResolver.tsx
@@ -2,12 +2,16 @@ import React from "react"
 
 import { ParagraphUnion } from "@/lib/graphql/generated/dpl-cms/graphql"
 
+import ParagraphGoMaterialSliderAutomatic from "./MaterialSlider/MaterialSliderAutomatic"
+import ParagraphGoMaterialSliderManual from "./MaterialSlider/MaterialSliderManual"
 import ParagraphGoVideo from "./ParagraphGoVideo/ParagraphGoVideo"
 import { ParagraphErrorBoundary } from "./paragraphErrorBoundary/paragraphErrorBoundary"
 
 function ParagraphResolver({ paragraphs }: { paragraphs: ParagraphUnion[] }) {
   const components = {
     ParagraphGoVideo,
+    ParagraphGoMaterialSliderAutomatic,
+    ParagraphGoMaterialSliderManual,
   }
 
   return paragraphs.map((paragraph, index) => {
