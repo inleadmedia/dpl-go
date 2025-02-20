8dc3eb958c8f9ffab40cf08c5cb2e3bd3feb5545
Jacob Pihl <jacob@reload.dk>
2025-02-20 14:21:25 +0100

    Extend search test with facets test

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -26,7 +26,31 @@ describe("Search Result Tests", () => {
     // Search for harry potter and press enter
     cy.dataCy("search-input").should("exist").focus().type("harry potter{enter}")
 
+  it("Should get results when searching", () => {
     // Check if search results are displayed
     cy.dataCy("work-card").should("have.length.above", 6)
+  })
+
+  it("Should have working facets", () => {
+    // Check if facets are displayed
+    cy.dataCy("filters-button").should("exist").click()
+
+    // Check if facets are displayed
+    cy.dataCy("filter-button").should("have.length.above", 40)
+
+    // Intercept search request with only one result
+    cy.interceptGraphql({
+      operationName: "searchWithPagination",
+      data: SearchWithPaginationFactory.transient({ hitcount: 1 }).build(),
+    })
+
+    // Select a facet
+    cy.dataCy("filter-button").first().click()
+
+    // Check if facet is selected
+    cy.dataCy("filter-button").should("have.length", 1)
+
+    // Check that only one result is displayed
+    cy.dataCy("work-card").should("have.length", 1)
   })
 })
