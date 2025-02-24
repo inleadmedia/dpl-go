5fb212178825056a95f50dbaa6cd04ad949c5676
thomasgross <thomasgrossrasmussen@gmail.com>
2025-02-24 17:12:11 +0100

    refactor: rename GRAPHQL_HOSTNAME to DPL_CMS_HOSTNAME in environment files and update usage in configuration

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: env.NEXT_PUBLIC_GRAPHQL_HOSTNAME || "",
+        hostname: env.NEXT_PUBLIC_DPL_CMS_HOSTNAME || "",
         pathname: "/**",
       },
     ],
