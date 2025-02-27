008d964064a81eba9517b39b573cba7663b70832
thomasgross <thomasgrossrasmussen@gmail.com>
2025-02-27 11:25:16 +0100

    fix: codegen

--- lib/graphql/generated/fbi/graphql.tsx
+++ lib/graphql/generated/fbi/graphql.tsx
@@ -1159,6 +1159,14 @@ export type NoteTypeEnum =
   | 'TECHNICAL_REQUIREMENTS'
   | 'TYPE_OF_SCORE';
 
+export type OrderTypeEnum =
+  | 'ESTIMATE'
+  | 'HOLD'
+  | 'LOAN'
+  | 'NON_RETURNABLE_COPY'
+  | 'NORMAL'
+  | 'STACK_RETRIEVAL';
+
 export type Pegi = {
   __typename?: 'PEGI';
   /** Display string for PEGI minimum age */
@@ -2440,7 +2448,7 @@ export const useSearchWithPaginationQuery = <
       variables: SearchWithPaginationQueryVariables,
       options?: Omit<UseQueryOptions<SearchWithPaginationQuery, TError, TData>, 'queryKey'> & { queryKey?: UseQueryOptions<SearchWithPaginationQuery, TError, TData>['queryKey'] }
     ) => {
-
+    
     return useQuery<SearchWithPaginationQuery, TError, TData>(
       {
     queryKey: ['searchWithPagination', variables],
@@ -2458,7 +2466,7 @@ export const useSuspenseSearchWithPaginationQuery = <
       variables: SearchWithPaginationQueryVariables,
       options?: Omit<UseSuspenseQueryOptions<SearchWithPaginationQuery, TError, TData>, 'queryKey'> & { queryKey?: UseSuspenseQueryOptions<SearchWithPaginationQuery, TError, TData>['queryKey'] }
     ) => {
-
+    
     return useSuspenseQuery<SearchWithPaginationQuery, TError, TData>(
       {
     queryKey: ['searchWithPaginationSuspense', variables],
@@ -2489,7 +2497,7 @@ export const useSearchFacetsQuery = <
       variables: SearchFacetsQueryVariables,
       options?: Omit<UseQueryOptions<SearchFacetsQuery, TError, TData>, 'queryKey'> & { queryKey?: UseQueryOptions<SearchFacetsQuery, TError, TData>['queryKey'] }
     ) => {
-
+    
     return useQuery<SearchFacetsQuery, TError, TData>(
       {
     queryKey: ['searchFacets', variables],
@@ -2507,7 +2515,7 @@ export const useSuspenseSearchFacetsQuery = <
       variables: SearchFacetsQueryVariables,
       options?: Omit<UseSuspenseQueryOptions<SearchFacetsQuery, TError, TData>, 'queryKey'> & { queryKey?: UseSuspenseQueryOptions<SearchFacetsQuery, TError, TData>['queryKey'] }
     ) => {
-
+    
     return useSuspenseQuery<SearchFacetsQuery, TError, TData>(
       {
     queryKey: ['searchFacetsSuspense', variables],
@@ -2539,7 +2547,7 @@ export const useComplexSearchForWorkTeaserQuery = <
       variables: ComplexSearchForWorkTeaserQueryVariables,
       options?: Omit<UseQueryOptions<ComplexSearchForWorkTeaserQuery, TError, TData>, 'queryKey'> & { queryKey?: UseQueryOptions<ComplexSearchForWorkTeaserQuery, TError, TData>['queryKey'] }
     ) => {
-
+    
     return useQuery<ComplexSearchForWorkTeaserQuery, TError, TData>(
       {
     queryKey: ['complexSearchForWorkTeaser', variables],
@@ -2557,7 +2565,7 @@ export const useSuspenseComplexSearchForWorkTeaserQuery = <
       variables: ComplexSearchForWorkTeaserQueryVariables,
       options?: Omit<UseSuspenseQueryOptions<ComplexSearchForWorkTeaserQuery, TError, TData>, 'queryKey'> & { queryKey?: UseSuspenseQueryOptions<ComplexSearchForWorkTeaserQuery, TError, TData>['queryKey'] }
     ) => {
-
+    
     return useSuspenseQuery<ComplexSearchForWorkTeaserQuery, TError, TData>(
       {
     queryKey: ['complexSearchForWorkTeaserSuspense', variables],
@@ -2586,7 +2594,7 @@ export const useGetMaterialQuery = <
       variables: GetMaterialQueryVariables,
       options?: Omit<UseQueryOptions<GetMaterialQuery, TError, TData>, 'queryKey'> & { queryKey?: UseQueryOptions<GetMaterialQuery, TError, TData>['queryKey'] }
     ) => {
-
+    
     return useQuery<GetMaterialQuery, TError, TData>(
       {
     queryKey: ['getMaterial', variables],
@@ -2604,7 +2612,7 @@ export const useSuspenseGetMaterialQuery = <
       variables: GetMaterialQueryVariables,
       options?: Omit<UseSuspenseQueryOptions<GetMaterialQuery, TError, TData>, 'queryKey'> & { queryKey?: UseSuspenseQueryOptions<GetMaterialQuery, TError, TData>['queryKey'] }
     ) => {
-
+    
     return useSuspenseQuery<GetMaterialQuery, TError, TData>(
       {
     queryKey: ['getMaterialSuspense', variables],
