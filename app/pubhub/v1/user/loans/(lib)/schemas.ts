a209e515346df8f478f5fa1fb0eaa8ebe682f641
Adam Antal <adoantal@gmail.com>
2025-03-05 11:19:47 +0100

    Enhance Zod schemas to validate date and number formats

--- app/pubhub/v1/user/loans/(lib)/schemas.ts
+++ app/pubhub/v1/user/loans/(lib)/schemas.ts
@@ -1,10 +1,12 @@
 import { z } from "zod"
 
+const regexDate = /^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}[\.0-9]*$/
+
 // Zod schemas
 export const orderItemSchema = z.object({
   retailerordernumber: z.string(),
-  orderdate: z.string(),
-  loanexpiredate: z.string(),
+  orderdate: z.string().regex(regexDate),
+  loanexpiredate: z.string().regex(regexDate),
   book: z.object({
     attributes: z.object({
       id: z.string(),
@@ -16,9 +18,9 @@ export const libraryUserOrderListSchema = z.object({
   response: z.object({
     status: z.object({
       LibraryExtension: z.object({
-        usertotalloans: z.string(),
-        usertotalebookloans: z.string(),
-        usertotalsoundloans: z.string(),
+        usertotalloans: z.string().regex(/^[0-9]+$/),
+        usertotalebookloans: z.string().regex(/^[0-9]+$/),
+        usertotalsoundloans: z.string().regex(/^[0-9]+$/),
       }),
     }),
     data: z.object({
