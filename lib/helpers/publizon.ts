38831a6fd08e329cb654c1109ccdfebb1aa09725
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2025-03-06 18:27:02 +0100

    Comment about md5 hash

--- lib/helpers/publizon.ts
+++ lib/helpers/publizon.ts
@@ -4,6 +4,8 @@ export const getPublizonServiceParameters = () => {
   return {
     clientid: process.env.UNLILOGIN_PUBHUB_CLIENT_ID ?? "",
     retailerid: process.env.UNLILOGIN_PUBHUB_RETAILER_ID ?? "",
+    // The  Publizon services expect the retailerkeycode to be an MD5 hash
+    // of the actual key code.
     retailerkeycode: md5(process.env.UNLILOGIN_PUBHUB_RETAILER_KEY_CODE ?? ""),
   }
 }
