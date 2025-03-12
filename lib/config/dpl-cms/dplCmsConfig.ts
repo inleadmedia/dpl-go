986c301c4cae1bbe400eef83feaf6881157158d6
Jacob Pihl <jacob@youtech.dk>
2025-03-12 15:54:43 +0100

    Refactor `dplCmsConfig`
    Prevously we would always try to fetch the config, even though it could be overridden by env variables. This would cause unwanted errors, as in CI we will never be able to fetch from the CMS, and this would throw errors.

--- lib/config/dpl-cms/dplCmsConfig.ts
+++ lib/config/dpl-cms/dplCmsConfig.ts
@@ -1,5 +1,7 @@
 import { useGetDplCmsConfigurationQuery } from "@/lib/graphql/generated/dpl-cms/graphql"
 
+import { getServerEnv } from "../env"
+
 const queryDplCmsConfig = async () => {
   const { goConfiguration } = await useGetDplCmsConfigurationQuery.fetcher(undefined, {
     next: { revalidate: 30 },
@@ -9,18 +11,27 @@ const queryDplCmsConfig = async () => {
 }
 
 export const getDplCmsUniloginConfig = async () => {
+  const wellknownUrl = getServerEnv("UNILOGIN_WELLKNOWN_URL")
+  const clientId = getServerEnv("UNILOGIN_CLIENT_ID")
+  const clientSecret = getServerEnv("UNILOGIN_CLIENT_SECRET")
+
+  // If all env vars are present, return them directly without fetching config
+  if (wellknownUrl && clientId && clientSecret) {
+    return {
+      wellknownUrl,
+      clientId,
+      clientSecret,
+      apiData: null,
+    }
+  }
+
+  // Only fetch config if we're missing some env vars
   const config = await queryDplCmsConfig()
 
   return {
-    wellknownUrl: process.env.UNILOGIN_WELLKNOWN_URL
-      ? process.env.UNILOGIN_WELLKNOWN_URL
-      : (config?.unilogin?.unilogin_api_wellknown_url ?? null),
-    clientId: process.env.UNILOGIN_CLIENT_ID
-      ? process.env.UNILOGIN_CLIENT_ID
-      : (config?.unilogin?.unilogin_api_client_id ?? null),
-    clientSecret: process.env.UNILOGIN_CLIENT_SECRET
-      ? process.env.UNILOGIN_CLIENT_SECRET
-      : (config?.unilogin?.unilogin_api_client_secret ?? null),
+    wellknownUrl: config?.unilogin?.unilogin_api_wellknown_url ?? null,
+    clientId: config?.unilogin?.unilogin_api_client_id ?? null,
+    clientSecret: config?.unilogin?.unilogin_api_client_secret ?? null,
     apiData: config?.unilogin ?? null,
   }
 }
