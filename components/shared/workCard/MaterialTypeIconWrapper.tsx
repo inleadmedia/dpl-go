4a7aa7c6207dd707345568949651072b0add55be
Adam Antal <adoantal@gmail.com>
2025-03-12 13:31:19 +0100

    Add className prop to MaterialTypeIconWrapper.tsx
    For improved flexibility of the component

--- components/shared/workCard/MaterialTypeIconWrapper.tsx
+++ components/shared/workCard/MaterialTypeIconWrapper.tsx
@@ -4,18 +4,23 @@ import { cn } from "@/lib/helpers/helper.cn"
 
 import Icon from "../icon/Icon"
 
+export type MaterialTypeIconWrapperProps = {
+  iconName: "book" | "headphones" | "controller" | "video" | "podcast"
+  costFree?: boolean
+  className?: string
+}
+
 const MaterialTypeIconWrapper = ({
   iconName,
   costFree,
-}: {
-  iconName: "book" | "headphones" | "controller" | "video" | "podcast"
-  costFree?: boolean
-}) => {
+  className,
+}: MaterialTypeIconWrapperProps) => {
   return (
     <div
       className={cn(
         "h-6 w-6 rounded-full md:h-10 md:w-10",
-        costFree ? "bg-content-blue-50" : "bg-background-overlay"
+        costFree ? "bg-content-blue-50" : "bg-background-overlay",
+        className
       )}>
       <div className="flex h-full w-full items-center justify-center">
         <Icon className="h-full w-full" name={iconName} />
