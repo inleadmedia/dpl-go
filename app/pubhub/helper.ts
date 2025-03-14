4b63f16e08a07ae5be661b8d156524818b80534b
Adam Antal <adoantal@gmail.com>
2025-03-14 14:26:48 +0100

    Edit regexDate to match all possible SOAP publizon date formats

--- app/pubhub/helper.ts
+++ app/pubhub/helper.ts
@@ -2,5 +2,5 @@ export const transformTimeToUtcString = (timeString: string) => {
   return timeString.split(" ").join("T")
 }
 
-export const regexDate = /^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}[\.0-9]*$/
+export const regexDate = /^[0-9]{4}-[0-9]{2}-[0-9]{2}[ T][0-9]{2}:[0-9]{2}:[0-9]{2}[\.0-9A-Z]*$/
 
