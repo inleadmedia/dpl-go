b08163d825e4ef7601e138adff6129f4a7d1a469
Adam Antal <adoantal@gmail.com>
2025-03-24 12:06:07 +0100

    Refactor loan creation logic to use identifier from context and remove unused helper function

--- app/pubhub/helper.ts
+++ app/pubhub/helper.ts
@@ -3,7 +3,3 @@ export const transformTimeToUtcString = (timeString: string) => {
 }
 
 export const regexDate = /^[0-9]{4}-[0-9]{2}-[0-9]{2}[ T][0-9]{2}:[0-9]{2}:[0-9]{2}[\.0-9A-Z]*$/
-
-export const getLastValueFromUrl = (url: string) => {
-  return url.slice(url.lastIndexOf("/") + 1)
-}
