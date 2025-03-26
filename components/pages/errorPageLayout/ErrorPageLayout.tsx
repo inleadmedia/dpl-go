265a3d851e5f38ac97cc377f66b0f467b73b5f20
thomasgross <thomasgrossrasmussen@gmail.com>
2025-03-26 13:24:00 +0100

    feat: aligned error pages

--- components/pages/errorPageLayout/ErrorPageLayout.tsx
+++ components/pages/errorPageLayout/ErrorPageLayout.tsx
@@ -1,15 +1,29 @@
-import React, { ReactNode } from "react"
+import Link from "next/link"
+import React from "react"
 
-const ErrorPageLayout = ({ lines }: { lines: (string | ReactNode)[] }) => {
+import { Button } from "@/components/shared/button/Button"
+
+const ErrorPageLayout = ({
+  title,
+  description,
+  buttonText,
+  buttonLink,
+}: {
+  title: string
+  description: string
+  buttonText: string
+  buttonLink: string
+}) => {
   return (
-    <div className="content-container flex items-center justify-center">
-      <div className="bg-white p-8 text-center shadow-md">
-        <h1 className="text-typo-heading-3 mb-6 font-bold">{lines[0]}</h1>
-        {lines.slice(1).map((line, index) => (
-          <p key={index} className="mb-6">
-            {line}
-          </p>
-        ))}
+    <div className="content-container grid-go w-full">
+      <div className="flex-column lg:space-y-grid-gap-1 my-grid-gap-2 space-y-grid-gap-2 lg:my-grid-gap-half col-span-10">
+        <h2 className="text-typo-huge">{title}</h2>
+        <p className="text-typo-body-lg max-w-prose">{description}</p>
+        <Button ariaLabel={buttonText} asChild size={"lg"}>
+          <Link href={buttonLink} className="text-typo-body-lg">
+            {buttonText}
+          </Link>
+        </Button>
       </div>
     </div>
   )
