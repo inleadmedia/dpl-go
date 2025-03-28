5417c3a30ee3afe2835aab73851073d91d955b22
Jacob Pihl <jacob@reload.dk>
2025-03-28 14:21:03 +0100

    Circumvent dplCms fetchers while in test mode
    Somehow Nextjs does not allow overriding NODE_ENV, so I've decided to add a new env variable to determine if TEST_MODE is active.

--- lib/config/dpl-cms/useGetDplCmsLoginUrls.ts
+++ lib/config/dpl-cms/useGetDplCmsLoginUrls.ts
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 
 import { GetLoginUrlsQuery, useGetLoginUrlsQuery } from "@/lib/graphql/generated/dpl-cms/graphql"
 
+import { getEnv } from "../env"
+
 const queryDplCmsConfig = async () => {
   const { goConfiguration } = await useGetLoginUrlsQuery.fetcher(undefined, {
     // @todo As a part of the caching initiative we should find out what to do here.
@@ -22,7 +24,9 @@ export default function useGetDplCmsLoginUrls() {
     }
 
     const fetchData = async () => {
-      const fetchedConfig = await queryDplCmsConfig()
+      // If we are running in test mode, we don't want to query the DPL CMS config
+      // TODO: The request should interceptet or overwitten by env variable
+      const fetchedConfig = getEnv("TEST_MODE") ? null : await queryDplCmsConfig()
       if (fetchedConfig !== null) {
         setConfig(fetchedConfig ?? null)
       }
