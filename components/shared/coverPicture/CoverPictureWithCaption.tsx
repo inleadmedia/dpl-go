d0bc343e7ad63edae2e8ed5238167e72c4a05a8d
Adam Antal <adoantal@gmail.com>
2025-04-02 11:40:47 +0200

    Correct typo in CoverPictureWithCaption

--- components/shared/coverPicture/CoverPictureWithCaption.tsx
+++ components/shared/coverPicture/CoverPictureWithCaption.tsx
@@ -15,7 +15,7 @@ const CoverPictureWithCaption = ({
 }: CoverPictureWithCaptionProps) => {
   return (
     <div className={cn("block h-full w-full space-y-3 px-[15%]", className)}>
-      <div className="reltive h-[85%]">{children}</div>
+      <div className="relative h-[85%]">{children}</div>
       <p className="text-typo-subtitle-sm break-words opacity-50">{caption}</p>
     </div>
   )
