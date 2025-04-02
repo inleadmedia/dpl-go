b3e24f0f6e0b3b062f4c617b8ca4446e4787302a
Adam Antal <adoantal@gmail.com>
2025-04-02 10:55:27 +0200

    Add skeleton component for ProfilePageLayout

--- app/user/profile/ProfilePageLayout.tsx
+++ app/user/profile/ProfilePageLayout.tsx
@@ -4,6 +4,7 @@ import React, { Suspense } from "react"
 
 import DebuggingSession from "@/app/user/profile/DebuggingSession"
 import LoanList from "@/app/user/profile/LoanList"
+import { LoanListSliderSkeleton } from "@/app/user/profile/LoanListSlider"
 import LogoutButton from "@/app/user/profile/LogoutButton"
 import { ButtonSkeleton } from "@/components/shared/button/Button"
 
@@ -21,6 +22,25 @@ const ProfilePageLayout = () => {
       </div>
       <LoanList />
       <DebuggingSession />
+    </div>
+  )
+}
+
+export const ProfilePageLayoutSkeleton = () => {
+  return (
+    <div className="content-container grid-go -mt-space-y w-full space-y-3">
+      <div className="col-span-full flex flex-row flex-wrap">
+        <h1 className="text-typo-subtitle-sm mb-5 lg:w-full">Profile</h1>
+        <ButtonSkeleton size="sm" className="ml-auto justify-end lg:order-2" />
+        {/* username skeleton */}
+        <div
+          className="text-typo-heading-2 bg-background-skeleton h-[46px] w-full animate-pulse rounded-sm lg:h-[66px]
+            lg:w-96"
+        />
+      </div>
+      <div className="col-span-full">
+        <LoanListSliderSkeleton />
+      </div>
     </div>
   )
 }
