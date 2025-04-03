0e75e0f1a045fdcb659408821be0bbf7806933d8
Jacob Pihl <jacob@reload.dk>
2025-04-03 14:55:40 +0200

    Intercept getAdgangsplatformenLibraryToken request before every Cypress test
    The application will try to fetch a library token whenever its missing a cookie for it.
    By having a global beforeEach we can make sure Cypress can mock the data whenever neccessary.

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,1 +1,14 @@
+import GetAdgangsplatformenLibraryToken from "../factories/getAdgangsplatformenLibraryToken"
 import "./commands"
+
+beforeEach(() => {
+  // Mock the library token request
+  cy.task("mockApiResponse", {
+    operationName: "getAdgangsplatformenLibraryToken",
+    data: GetAdgangsplatformenLibraryToken.build(),
+  })
+})
+
+afterEach(() => {
+  cy.task("resetApiMocks")
+})
