a7cfaef761fcbf04f136ec8a7b58ae4503cd3c29
Jacob Pihl <jacob@reload.dk>
2025-04-03 14:53:16 +0200

    Added factory to mock fetched library token

---
+++ cypress/factories/getAdgangsplatformenLibraryToken.ts
@@ -0,0 +1,19 @@
+import dayjs from "dayjs"
+import { Factory } from "fishery"
+
+import { GetAdgangsplatformenLibraryTokenQuery } from "@/lib/graphql/generated/dpl-cms/graphql"
+
+export default Factory.define<GetAdgangsplatformenLibraryTokenQuery>(() => {
+  return {
+    dplTokens: {
+      adgangsplatformen: {
+        library: {
+          token: "library-token",
+          expire: {
+            timestamp: dayjs().add(1, "day").unix(),
+          },
+        },
+      },
+    },
+  }
+})
