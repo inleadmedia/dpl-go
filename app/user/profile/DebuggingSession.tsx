16c40734ec589e2380d49e124f93f6338e7aae80
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2025-04-08 16:03:16 +0200

    Add server time so we can compare expire timestamps with current time

--- app/user/profile/DebuggingSession.tsx
+++ app/user/profile/DebuggingSession.tsx
@@ -11,6 +11,8 @@ const DebuggingSession = () => {
       <h2 className="text-typo-heading-5 col-span-full mt-40">Debugging:</h2>
       <div className="col-span-full mt-3">
         <pre>{JSON.stringify(session, null, 2)}</pre>
+        <hr className="my-10" />
+        <pre>Current server time: {new Date().toISOString()}</pre>
       </div>
     </>
   )
