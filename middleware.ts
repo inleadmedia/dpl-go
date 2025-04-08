ea2cc4143ba14088851fe99ee37ad48601357ae3
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2025-04-08 12:55:55 +0200

    Make sure that middleware does not try to refresh unilogin token
    ...adter it has destroyed the session because of overdue refresh time

--- middleware.ts
+++ middleware.ts
@@ -60,6 +60,7 @@ export async function middleware(request: NextRequest) {
 
   if (uniloginAccessTokenHasExpired(session)) {
     destroySession(session)
+    return response
   }
 
   if (uniloginAccessTokenShouldBeRefreshed(session)) {
