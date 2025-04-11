7788ddc26bb12b87dac9cba0ce8ad90487798abe
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2025-04-11 08:41:02 +0200

    Add rendering of username on profile page

--- app/user/profile/ProfilePageLayout.tsx
+++ app/user/profile/ProfilePageLayout.tsx
@@ -6,6 +6,8 @@ import LogoutButton from "@/app/user/profile/LogoutButton"
 import UserLoans from "@/app/user/profile/UserLoans"
 import { ButtonSkeleton } from "@/components/shared/button/Button"
 
+import Username, { UsernameSkeleton } from "./Username"
+
 const ProfilePageLayout = () => {
   return (
     <div className="content-container grid-go -mt-space-y w-full space-y-3">
@@ -14,9 +16,7 @@ const ProfilePageLayout = () => {
         <Suspense fallback={<ButtonSkeleton size="sm" />}>
           <LogoutButton />
         </Suspense>
-        <p className="text-typo-heading-2 mt-6 w-full pb-5 lg:order-1 lg:mt-0 lg:w-auto lg:max-w-[80%]">
-          Username / User Name
-        </p>
+        <Username />
       </div>
       <UserLoans />
       <DebuggingSession />
@@ -30,7 +30,7 @@ export const ProfilePageLayoutSkeleton = () => {
       <div className="col-span-full flex flex-row flex-wrap">
         <h1 className="text-typo-subtitle-sm mb-5 lg:w-full">Profile</h1>
         <ButtonSkeleton size="sm" className="ml-auto justify-end lg:order-2" />
-        {/* username skeleton */}
+        <UsernameSkeleton />
         <div
           className="text-typo-heading-2 bg-background-skeleton h-[46px] w-full animate-pulse rounded-sm lg:h-[66px]
             lg:w-96"
