d516a2b5e5952bf63e1b6d7deb49c3229ce5da46
Adam Antal <adoantal@gmail.com>
2025-04-23 09:09:33 +0200

    Render support ID on the profile page

--- app/user/profile/ProfilePageLayout.tsx
+++ app/user/profile/ProfilePageLayout.tsx
@@ -8,6 +8,7 @@ import { ButtonSkeleton } from "@/components/shared/button/Button"
 import { userIsAnonymous } from "@/lib/helpers/user"
 import { getSession } from "@/lib/session/session"
 
+import SupportId, { SupportIdSkeleton } from "./SupportId"
 import Username, { UsernameSkeleton } from "./Username"
 
 const ProfilePageLayout = async () => {
@@ -20,7 +21,7 @@ const ProfilePageLayout = async () => {
   return (
     <div className="content-container grid-go -mt-space-y w-full space-y-3">
       <div className="col-span-full flex flex-row flex-wrap">
-        <h1 className="text-typo-subtitle-sm mb-5 lg:w-full">Profile</h1>
+        <h1 className="text-typo-subtitle-sm mb-5 opacity-50 lg:w-full">Profile</h1>
         <Suspense fallback={<ButtonSkeleton size="sm" />}>
           <LogoutButton />
         </Suspense>
@@ -28,6 +29,9 @@ const ProfilePageLayout = async () => {
           <Username />
         </Suspense>
       </div>
+      <Suspense fallback={<SupportIdSkeleton />}>
+        <SupportId />
+      </Suspense>
       <Suspense fallback={<LoanSliderSkeleton />}>
         <UserLoans />
       </Suspense>
