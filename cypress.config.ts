3cae72375dc44e8f71886cde9119aa50f1aae9f2
Jacob Pihl <jacob@youtech.dk>
2025-05-07 14:02:09 +0200

    Use .env.test inside cypress
    This makes it possible for cypress to access env data, which can be used as part of mocked data inside tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,21 @@
+import { loadEnvConfig } from "@next/env"
 import { defineConfig } from "cypress"
 
 import { e2eNodeEvents } from "./cypress/support/setupNodeEvents/index"
 
+// Load environment variables from .env.test
+const { combinedEnv } = loadEnvConfig(process.cwd(), true)
+
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    setupNodeEvents: e2eNodeEvents,
+    setupNodeEvents(on, config) {
+      config.env = {
+        ...combinedEnv,
+        ...config.env,
+      }
+      return e2eNodeEvents?.(on, config) ?? config
+    },
     experimentalInteractiveRunEvents: true,
   },
 })
