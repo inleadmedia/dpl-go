db2851840fa8c0f13729bb4a78a79d4af753a61c
thomasgross <thomasgrossrasmussen@gmail.com>
2025-05-08 16:08:23 +0200

    fix: missing paragraphs not showing layouts

--- components/pages/basicPageLayout/BasicPageLayout.tsx
+++ components/pages/basicPageLayout/BasicPageLayout.tsx
@@ -6,13 +6,9 @@ import { NodeGoPage } from "@/lib/graphql/generated/dpl-cms/graphql"
 function BasicPageLayout({ pageData }: { pageData: NodeGoPage }) {
   const { paragraphs } = pageData
 
-  if (!paragraphs) {
-    return null
-  }
-
   return (
     <div className="gap-y-paragraph-spacing flex w-full flex-col">
-      <ParagraphResolver paragraphs={paragraphs} />
+      {paragraphs && <ParagraphResolver paragraphs={paragraphs} />}
     </div>
   )
 }
