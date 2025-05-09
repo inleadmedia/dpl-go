b84868fb293007e8107b35dc60cc6a35d9fbb8e4
thomasgross <thomasgrossrasmussen@gmail.com>
2025-05-09 14:49:47 +0200

    feat: cleanup code and type comparison on status code

--- app/(routes)/pubhub/v1/user/loans/[identifier]/route.ts
+++ app/(routes)/pubhub/v1/user/loans/[identifier]/route.ts
@@ -3,32 +3,39 @@ import { NextRequest, NextResponse } from "next/server"
 import { withAuth } from "@/app/(routes)/pubhub/(lib)/helper"
 import { TUserInfo } from "@/app/(routes)/pubhub/(lib)/types"
 import { transformTimeToUtcString } from "@/app/(routes)/pubhub/helper"
+import { ApiResponseCode } from "@/lib/rest/publizon/local-adapter/generated/model"
 
 import { createLoanRequest } from "./(lib)/requests"
 import { createLoanSchema } from "./(lib)/schemas"
+
+const createLoan = createLoanSchema.transform(loanData => {
+  const response = loanData.response
+  const { expirationdateutc, internalordernumber, retailerordernumber } = response.data || {}
+  const { code, message } = response.status
+
+  // Only add the variable if it exists in the response
+  return {
+    ...(expirationdateutc && { expirationDateUtc: transformTimeToUtcString(expirationdateutc) }),
+    ...(internalordernumber && { orderNumber: internalordernumber }),
+    ...(retailerordernumber && { orderId: retailerordernumber }),
+    code,
+    message,
+  }
+})
 
 async function createdigitalLoan(
   request: NextRequest,
   context: { uniLoginUserInfo: TUserInfo; params: { identifier: string } }
 ) {
   const { identifier } = await context.params
-  const createLoan = createLoanSchema.transform(loanData => {
-    const response = loanData.response
-    return {
-      expirationDateUtc: transformTimeToUtcString(response.data?.expirationdateutc || ""),
-      orderNumber: response.data?.internalordernumber,
-      orderId: response.data?.retailerordernumber,
-      code: response.status.code,
-      message: `${response.status.message} (#${response.status.code})`,
-    }
-  })
+
   try {
     const responseData = await createLoanRequest(context.uniLoginUserInfo, identifier)
     const data = createLoan.parse(responseData)
 
     // Check if the response code is not "101" (OK)
     // https://docs.pubhub.dk/LibraryApi/2.7/Pubhub_Library_Integration_Guide_v2.7_September_2023.pdf
-    if (data.code !== "101") {
+    if (Number(data.code) !== ApiResponseCode.NUMBER_101) {
       return NextResponse.json(data, { status: 400 })
     }
 
