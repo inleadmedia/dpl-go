8a6705b6da14561610a06f6b673abef3427bf48a
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2025-05-14 16:39:24 +0200

    Make common way of choosing redirect or rendering on cms pages
    By wrapping the various page components we can centralise the
    functionality.

--- app/(pages-with-category-slider)/kategori/[...slug]/page.tsx
+++ app/(pages-with-category-slider)/kategori/[...slug]/page.tsx
@@ -1,8 +1,9 @@
-import { notFound, redirect } from "next/navigation"
 import React from "react"
 
+import RedirectNotFoundOrRenderPage from "@/components/global/dplCmsPage/RedirectNotFoundOrRenderPage"
 import CategoryPageLayout from "@/components/pages/categoryPageLayout/CategoryPageLayout"
 import { NodeGoCategory } from "@/lib/graphql/generated/dpl-cms/graphql"
+import { getEntityFromPageData } from "@/lib/helpers/dpl-cms-content"
 import { setPageMetadata } from "@/lib/helpers/helper.metadata"
 
 import loadCategoryPage from "./loadCategoryPage"
@@ -26,27 +27,13 @@ export async function generateMetadata(props: { params: Promise<{ slug: string[]
 
 async function page(props: { params: Promise<{ slug: string[] }> }) {
   const data = await getPage((await props.params).slug)
+  const entity = getEntityFromPageData(data)
 
-  const routeType = data.route?.__typename
-
-  if (routeType === "RouteRedirect") {
-    if (data.route?.url) {
-      return redirect(data.route.url)
-    }
-  }
-  if (routeType === "RouteExternal") {
-    // TODO: implement external route redirect
-    return notFound()
-  }
-  if (routeType === "RouteInternal") {
-    const { entity } = data.route ?? {}
-
-    if (entity?.__typename === "NodeGoCategory") {
-      return <CategoryPageLayout pageData={entity as NodeGoCategory} />
-    }
-  }
-
-  return notFound()
+  return (
+    <RedirectNotFoundOrRenderPage data={data} pageType="NodeGoCategory">
+      <CategoryPageLayout pageData={entity as NodeGoCategory} />
+    </RedirectNotFoundOrRenderPage>
+  )
 }
 
 export default page
--- lib/helpers/dpl-cms-content.ts
+++ lib/helpers/dpl-cms-content.ts
@@ -1,3 +1,5 @@
+import { GetPageByPathQuery } from "../graphql/generated/dpl-cms/graphql"
+
 type TDplCmsContentType = "article" | "page" | "category"
 
 export const getContentQueryPath = (path: string, type: TDplCmsContentType) => {
@@ -9,3 +11,6 @@ export const getContentQueryPath = (path: string, type: TDplCmsContentType) => {
 
   return pathMap[type] ?? path
 }
+
+export const getEntityFromPageData = (data: GetPageByPathQuery) =>
+  data.route?.__typename === "RouteInternal" ? data.route.entity : undefined
