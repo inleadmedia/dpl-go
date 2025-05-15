faa30e853045ba86df5ba1744834a320946ee683
thomasgross <thomasgrossrasmussen@gmail.com>
2025-05-15 14:37:12 +0200

    fix: remove unnecessary fallback for token in loadPreviewPage

--- app/preview/[id]/loadPreviewPage.ts
+++ app/preview/[id]/loadPreviewPage.ts
@@ -4,8 +4,8 @@ import { useGetPreviewPageByIddQuery } from "@/lib/graphql/generated/dpl-cms/gra
 const loadPreviewPage = async ({ id, token }: { id: string; token: string }) => {
   const queryClient = getQueryClient()
   const data = await queryClient.fetchQuery({
-    queryKey: useGetPreviewPageByIddQuery.getKey({ id: id, token: token || "" }),
-    queryFn: useGetPreviewPageByIddQuery.fetcher({ id: id, token: token || "" }),
+    queryKey: useGetPreviewPageByIddQuery.getKey({ id: id, token: token }),
+    queryFn: useGetPreviewPageByIddQuery.fetcher({ id: id, token: token }),
   })
 
   return data
