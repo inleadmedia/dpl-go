f46804cedb15655718121b2885883126c6ac3737
Adam Antal <adoantal@gmail.com>
2025-05-20 11:31:33 +0200

    Don't check whether user can loan more materials
    We opt in to make the call and let the service handle whether user can loan more or not. Then we show the message that comes back from the service in case they can't.

--- components/shared/loanMaterialModal/LoanMaterialModal.tsx
+++ components/shared/loanMaterialModal/LoanMaterialModal.tsx
@@ -2,11 +2,8 @@ import { useQueryClient } from "@tanstack/react-query"
 import React, { useState } from "react"
 
 import {
-  canUserLoanMoreCostFreeMaterials,
-  canUserLoanMoreMaterials,
   getManifestationMaterialTypeIcon,
   getManifestationMaterialTypeSpecific,
-  isManifestationPodcast,
 } from "@/components/pages/workPageLayout/helper"
 import { Button } from "@/components/shared/button/Button"
 import { CoverPicture, CoverPictureSkeleton } from "@/components/shared/coverPicture/CoverPicture"
@@ -19,10 +16,7 @@ import { getCoverUrls, getLowResCoverUrl } from "@/lib/helpers/helper.covers"
 import { getIsbnsFromManifestation } from "@/lib/helpers/ids"
 import { useGetCoverCollection } from "@/lib/rest/cover-service-api/generated/cover-service"
 import { GetCoverCollectionSizesItem } from "@/lib/rest/cover-service-api/generated/model"
-import { useGetV1ProductsIdentifierAdapter } from "@/lib/rest/publizon/adapter/generated/publizon"
 import { ApiResponseCode } from "@/lib/rest/publizon/local-adapter/generated/model"
-import useGetV1LibraryProfile from "@/lib/rest/publizon/useGetV1LibraryProfile"
-import useGetV1UserLoans from "@/lib/rest/publizon/useGetV1UserLoans"
 import usePostV1UserLoansIdentifier from "@/lib/rest/publizon/usePostV1UserLoansIdentifier"
 import { modalStore } from "@/store/modal.store"
 
@@ -58,12 +52,6 @@ const LoanMaterialModal = ({
     "small",
     "xx-small",
   ])
-  const { data: dataLoans, isLoading: isLoadingLoans, isError: isErrorLoans } = useGetV1UserLoans()
-  const {
-    data: dataLibraryProfile,
-    isLoading: isLoadingLibraryProfile,
-    isError: isErrorLibraryProfile,
-  } = useGetV1LibraryProfile()
   const { mutate } = usePostV1UserLoansIdentifier()
   const isbns = getIsbnsFromManifestation(manifestation)
   const [isHandlingLoan, setIsHandlingLoan] = useState(false)
@@ -93,35 +81,11 @@ const LoanMaterialModal = ({
       }
     )
   }
-
-  const { data: publizonData } = useGetV1ProductsIdentifierAdapter(isbns?.[0], {
-    // Publizon / useGetV1ProductsIdentifier is responsible for online
-    // materials. It requires an ISBN to do lookups.
-    enabled: isbns.length > 0,
-  })
-
-  const isCostFree = publizonData?.product?.costFree || isManifestationPodcast(manifestation)
-  // Check if the user can loan more e-materials or if the material is cost-free (blue title or podcast)
-  const isLoanPossible =
-    (isCostFree && canUserLoanMoreCostFreeMaterials(dataLoans)) ||
-    canUserLoanMoreMaterials(dataLoans, dataLibraryProfile, manifestation)
-
   const errors: string[] = []
-  if (!isLoanPossible) {
-    errors.push(
-      `Du kan desværre ikke låne flere titler af typen "${getManifestationMaterialTypeSpecific(manifestation)}" i denne måned.`
-    )
-  }
-  if (isErrorLoans || isErrorLibraryProfile) {
-    errors.push(
-      "Der sket desværre et fejl ved at checke om du kan låne materialet. Prøv igen senere."
-    )
-  }
+
   if (publizonError) {
     errors.push(publizonErrorMessageMap[publizonError.code] || publizonError.message)
   }
-
-  const isLoading = isLoadingLibraryProfile || isLoadingLoans
 
   return (
     <ResponsiveDialog
@@ -145,25 +109,19 @@ const LoanMaterialModal = ({
       </div>
 
       {/* Description */}
-      {isLoading && (
-        <div className="bg-background-skeleton mx-auto mt-10 mb-5 h-[26px] w-[500px] animate-pulse rounded-full" />
-      )}
-      {!isLoading && (
-        <div className="mx-auto mt-10 mb-5 w-full max-w-prose space-y-4">
-          <p className="text-typo-subtitle-md text-center">
-            {`Er du sikker på at du vil låne materialet${` (${getManifestationMaterialTypeSpecific(manifestation)})?` || "?"}`}
-          </p>
-
-          {errors.length > 0 && (
-            <div className="flex">
-              <div className="bg-error-red-100 text-error-red-400 rounded-base mx-auto flex items-center gap-4 p-4">
-                <Icon className={cn("h-5 min-h-5 w-5 min-w-5")} name="alert" />
-                <p className="text-typo-link">{errors[0]}</p>
-              </div>
+      <div className="mx-auto mt-10 mb-5 w-full max-w-prose space-y-4">
+        <p className="text-typo-subtitle-md text-center">
+          {`Er du sikker på at du vil låne materialet${` (${getManifestationMaterialTypeSpecific(manifestation)})?` || "?"}`}
+        </p>
+        {errors.length > 0 && (
+          <div className="flex">
+            <div className="bg-error-red-100 text-error-red-400 rounded-base mx-auto flex items-center gap-4 p-4">
+              <Icon className={cn("h-5 min-h-5 w-5 min-w-5")} name="alert" />
+              <p className="text-typo-link">{errors[0]}</p>
             </div>
-          )}
-        </div>
-      )}
+          </div>
+        )}
+      </div>
 
       <div className="flex flex-row items-center justify-center gap-6">
         {/* Only show "approve loan" button if user can still loan more materials */}
@@ -172,9 +130,9 @@ const LoanMaterialModal = ({
             theme={"primary"}
             size={"lg"}
             onClick={handleLoanMaterial}
-            disabled={isHandlingLoan || isLoading}>
-            {!isHandlingLoan && !isLoading && "Ja"}
-            {(isHandlingLoan || isLoading) && (
+            disabled={isHandlingLoan}>
+            {!isHandlingLoan && "Ja"}
+            {isHandlingLoan && (
               <Icon
                 name="go-spinner"
                 ariaLabel="Indlæser"
@@ -183,9 +141,9 @@ const LoanMaterialModal = ({
             )}
           </Button>
         )}
-        <Button size={"lg"} disabled={isHandlingLoan || isLoading} onClick={() => closeModal()}>
-          {errors.length ? "Luk" : "Nej"}
-          {isLoading && (
+        <Button size={"lg"} disabled={isHandlingLoan} onClick={() => closeModal()}>
+          {!isHandlingLoan && (errors.length ? "Luk" : "Nej")}
+          {isHandlingLoan && (
             <Icon
               name="go-spinner"
               ariaLabel="Indlæser"
