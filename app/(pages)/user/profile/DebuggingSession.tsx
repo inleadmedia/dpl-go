4f5fc1e9d17ee54882e5830421fea43b5c5e3e93
Jacob Pihl <jacob@youtech.dk>
2025-05-24 15:41:05 +0200

    Suppress hydration warning
    When using Date() in a react component it will render two different time slots on both the server and the client which will throw an error.
    
    https://nextjs.org/docs/messages/react-hydration-error

--- app/(pages)/user/profile/DebuggingSession.tsx
+++ app/(pages)/user/profile/DebuggingSession.tsx
@@ -12,7 +12,7 @@ const DebuggingSession = () => {
       <div className="col-span-full mt-3">
         <pre>{JSON.stringify(session, null, 2)}</pre>
         <hr className="my-10" />
-        <pre>Current server time: {new Date().toISOString()}</pre>
+        <pre suppressHydrationWarning>Current server time: {new Date().toISOString()}</pre>
       </div>
     </>
   )
