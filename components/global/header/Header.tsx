8128ce38d24f5943512975eff5a8458d38d869fd
thomasgross <thomasgrossrasmussen@gmail.com>
2025-06-02 10:54:51 +0200

    fix: Remove unused Suspense import

--- components/global/header/Header.tsx
+++ components/global/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react"
+import React from "react"
 
 import { Button } from "@/components/shared/button/Button"
 import DarkModeToggle from "@/components/shared/darkModeToggle/DarkModeToggle"
