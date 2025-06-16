116ae634f85c02fe05ae1d87f5a91a7f96b04e67
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2025-06-16 11:03:17 +0200

    Use potential www in go urls in startup script

--- lagoon/start.sh
+++ lagoon/start.sh
@@ -1,6 +1,13 @@
 # Set the environment variables.
 # These ones are varying from environment to environment.
-export NEXT_PUBLIC_APP_URL="https://go.${LAGOON_DOMAIN}"
+local GO_SUB_DOMAIN="go."
+# If primaryDomain uses www, then we want to put the go subdomain in there like this: www.go.restOfDomain.tld
+local PRIMARY_GO_DOMAIN="${GO_SUB_DOMAIN}${LAGOON_DOMAIN}"
+if [[ $LAGOON_DOMAIN == www* ]]; then
+  PRIMARY_GO_DOMAIN="${LAGOON_DOMAIN/www./www.${GO_SUB_DOMAIN}}"
+fi
+
+export NEXT_PUBLIC_APP_URL="https://${PRIMARY_GO_DOMAIN}"
 export NEXT_PUBLIC_DPL_CMS_HOSTNAME="${LAGOON_DOMAIN}"
 export NEXT_PUBLIC_GRAPHQL_SCHEMA_ENDPOINT_DPL_CMS="${LAGOON_ROUTE}/graphql"
 
