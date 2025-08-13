20521883f9e4c06a2045398adfc4a7ee283b279b
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2025-08-13 22:15:16 +0200

    Make redirect button optional in ErrorPageLayout component

--- components/pages/errorPageLayout/ErrorPageLayout.tsx
+++ components/pages/errorPageLayout/ErrorPageLayout.tsx
@@ -11,19 +11,21 @@ const ErrorPageLayout = ({
 }: {
   title: string
   description: React.ReactNode
-  buttonText: string
-  buttonLink: string
+  buttonText?: string
+  buttonLink?: string
 }) => {
   return (
     <div className="content-container grid-go w-full">
       <div className="flex-column lg:space-y-grid-gap-1 my-grid-gap-2 space-y-grid-gap-2 lg:my-grid-gap-half col-span-6">
         <h2 className="text-typo-heading-1">{title}</h2>
         <div className="wysiwyg">{description}</div>
-        <Button ariaLabel={buttonText} asChild size={"lg"}>
-          <Link href={buttonLink} className="text-typo-body-lg">
-            {buttonText}
-          </Link>
-        </Button>
+        {buttonLink && buttonText && (
+          <Button ariaLabel={buttonText} asChild size={"lg"}>
+            <Link href={buttonLink} className="text-typo-body-lg">
+              {buttonText}
+            </Link>
+          </Button>
+        )}
       </div>
     </div>
   )
