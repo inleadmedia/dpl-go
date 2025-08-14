5cf2196d6cab2150c5e67731df3f8b38f7515a6b
thomasgross <thomasgrossrasmussen@gmail.com>
2025-08-14 14:08:07 +0200

    feat: add health status route with version and frontpage data checks

---
+++ app/(routes)/health/route.ts
@@ -0,0 +1,61 @@
+import { promises as fs } from "fs"
+import { isEmpty } from "lodash"
+import { NextResponse } from "next/server"
+
+import goConfig from "@/lib/config/goConfig"
+import { loadPageData } from "@/lib/helpers/dpl-cms-content"
+
+type HealthStatusRequestBody = {
+  version: string
+  requests: {
+    [key: string]: {
+      status: "ok" | "error"
+      message: string
+    }
+  }
+}
+
+async function getHealthStatus() {
+  const requestBody: HealthStatusRequestBody = {
+    version: "",
+    requests: {},
+  }
+
+  try {
+    // Get version from .version json file in root of project. .version file should only exist in lagoon.
+    const jsonFile = await fs.readFile(".version", "utf8")
+    requestBody.version = JSON.parse(jsonFile).version
+  } catch (error) {
+    requestBody.version = `Error reading version: ${error instanceof Error ? error.message : "Unknown error"}`
+  }
+
+  try {
+    const frontpageData = await loadPageData({
+      contentPath: goConfig("routes.frontpage"),
+      type: "page",
+    })
+
+    if (!isEmpty(frontpageData)) {
+      requestBody.requests.frontpage = {
+        status: "ok",
+        message: "Frontpage data loaded successfully",
+      }
+    } else {
+      requestBody.requests.frontpage = {
+        status: "error",
+        message: `Frontpage data is empty`,
+      }
+    }
+  } catch (error) {
+    requestBody.requests.frontpage = {
+      status: "error",
+      message: `Error loading frontpage data: ${error instanceof Error ? error.message : "Unknown error"}`,
+    }
+  }
+
+  return NextResponse.json(requestBody, { status: 200 })
+}
+
+export const GET = getHealthStatus
+
+export const dynamic = "force-dynamic"
