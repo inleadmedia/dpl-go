5701057407f411d021b281dd1fb90f3a16a868de
thomasgross <thomasgrossrasmussen@gmail.com>
2025-08-25 15:01:29 +0200

    chore: update wsdl-tsclient dependency to use reload fork and refactor helper function

--- scripts/wsdl-tsclient/helper.ts
+++ scripts/wsdl-tsclient/helper.ts
@@ -1,21 +1,10 @@
-import prependFile from "prepend-file"
-import { parseAndGenerate } from "wsdl-tsclient"
+import { parseAndGenerate } from "@reloaddk/wsdl-tsclient"
 
 export const generateWsdlFiles = (wsdlFiles: string[]) => {
   wsdlFiles.forEach(wsdlFile => {
     const pattern = /(.*)\/wsdl\/([^\/]+)\.wsdl$/
     const operationId = wsdlFile.replace(pattern, "$2")
     const directory = wsdlFile.replace(pattern, "$1")
-
-    parseAndGenerate(`${directory}/wsdl/${operationId}.wsdl`, `${directory}/generated/`).then(
-      () => {
-        prependFile(
-          `${directory}/generated/${operationId}/index.ts`,
-          "// This is generated code thus no need for type checking...\r\n// @ts-nocheck\r\n"
-        ).then(() => {
-          console.log(`Generated files for ${operationId}`)
-        })
-      }
-    )
+    parseAndGenerate(`${directory}/wsdl/${operationId}.wsdl`, `${directory}/generated/`)
   })
 }
